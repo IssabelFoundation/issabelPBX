756394e5207a051233c7cc825d0592fb1123fac8
Nicolas Gudino <asternic@gmail.com>
2018-07-13 18:17:01 -0400

    Add HangupCause check for unallocated numbers and info tone

--- outroutemsg/functions.inc.php
+++ outroutemsg/functions.inc.php
@@ -1,12 +1,14 @@
 <?php /* $Id: function.inc.php  $ */
 if (!defined('ISSABELPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //	License for all code of this IssabelPBX module can be found in the license file inside the module directory
+//	Copyright 2018 Issabel Foundation
 //	Copyright 2013 Schmooze Com Inc.
 //  Copyright (C) 2010 Mikael Carlsson
 //
 
 define ('DEFAULT_MSG', -1);
 define ('CONGESTION_TONE', -2);
+define ('INFO_TONE', -3);
 
 function outroutemsg_get_config($engine) {
 	global $db;
@@ -32,6 +34,8 @@ function outroutemsg_get_config($engine) {
 		$ext->add($contextname, $exten, '', new ext_progress());
 		$ext->add($contextname, $exten, '', new ext_gotoif('$["${EMERGENCYROUTE}" = "YES"]', 'emergency,1'));
 		$ext->add($contextname, $exten, '', new ext_gotoif('$["${INTRACOMPANYROUTE}" = "YES"]', 'intracompany,1'));
+		$ext->add($contextname, $exten, '', new ext_gotoif('$["${HANGUPCAUSE}" = "1"]',  'unallocated,1'));
+		$ext->add($contextname, $exten, '', new ext_gotoif('$["${HANGUPCAUSE}" = "21"]', 'unallocated,1'));
 
 		switch ($outroutemsg_ids['default_msg_id']) {
 			case DEFAULT_MSG:
@@ -79,21 +83,39 @@ function outroutemsg_get_config($engine) {
 		}
 		$ext->add($contextname, $exten, '', new ext_congestion());
 		$ext->add($contextname, $exten, '', new ext_hangup());
+
+		$exten = 'unallocated';
+		switch ($outroutemsg_ids['unallocated_msg_id']) {
+			case DEFAULT_MSG:
+				$ext->add($contextname, $exten, '', new ext_playback("you-dialed-wrong-number"));
+				break;
+			case INFO_TONE:
+				$ext->add($contextname, $exten, '', new ext_playtones("info"));
+				break;
+			default:
+				$message = recordings_get_file($outroutemsg_ids['unallocated_msg_id']);
+				$message = ($message != "") ? $message : "you-dialed-wrong-number";
+				$ext->add($contextname, $exten, '', new ext_playback("$message"));
+		}
+		$ext->add($contextname, $exten, '', new ext_congestion());
+		$ext->add($contextname, $exten, '', new ext_hangup());
+
 	}
 }
 
-function outroutemsg_add($default_msg_id, $intracompany_msg_id, $emergency_msg_id, $no_answer_msg_id, $invalidnmbr_msg_id) {
+function outroutemsg_add($default_msg_id, $intracompany_msg_id, $emergency_msg_id, $no_answer_msg_id, $invalidnmbr_msg_id, $unallocated_msg_id) {
 	global $db;
 
 	$default_msg_id      = $db->escapeSimple($default_msg_id);
 	$intracompany_msg_id = $db->escapeSimple($intracompany_msg_id);
 	$emergency_msg_id    = $db->escapeSimple($emergency_msg_id);
+	$unallocated_msg_id  = $db->escapeSimple($unallocated_msg_id);
 	$no_answer_msg_id    = $db->escapeSimple($no_answer_msg_id);
 	$invalidnmbr_msg_id  = $db->escapeSimple($invalidnmbr_msg_id);
 	
 	// in future will do in a outroutemsg_del but not needed for now
 	//
-	$sql = "DELETE FROM outroutemsg WHERE `keyword` IN  ('default_msg_id', 'intracompany_msg_id', 'emergency_msg_id', 'no_answer_msg_id', 'invalidnmbr_msg_id')";
+	$sql = "DELETE FROM outroutemsg WHERE `keyword` IN  ('default_msg_id', 'intracompany_msg_id', 'emergency_msg_id', 'no_answer_msg_id', 'invalidnmbr_msg_id', 'unallocated_msg_id')";
 	$result = $db->query($sql);
 	if(DB::IsError($result)) {
 		die_issabelpbx($result->getMessage().$sql);
@@ -103,6 +125,7 @@ function outroutemsg_add($default_msg_id, $intracompany_msg_id, $emergency_msg_i
 		array('default_msg_id', "$default_msg_id"),
 		array('intracompany_msg_id', "$intracompany_msg_id"),
 		array('emergency_msg_id', "$emergency_msg_id"),
+		array('unallocated_msg_id', "$unallocated_msg_id"),
 		array('no_answer_msg_id', "$no_answer_msg_id"),
 		array('invalidnmbr_msg_id', "$invalidnmbr_msg_id"),
 		);
@@ -124,6 +147,7 @@ function outroutemsg_get() {
 	$results['default_msg_id']      = isset($results['default_msg_id'])      ? $results['default_msg_id']      : DEFAULT_MSG;
 	$results['intracompany_msg_id'] = isset($results['intracompany_msg_id']) ? $results['intracompany_msg_id'] : DEFAULT_MSG;
 	$results['emergency_msg_id']    = isset($results['emergency_msg_id'])    ? $results['emergency_msg_id']    : DEFAULT_MSG;
+	$results['unallocated_msg_id']  = isset($results['unallocated_msg_id'])  ? $results['unallocated_msg_id']  : DEFAULT_MSG;
 	$results['no_answer_msg_id']    = isset($results['no_answer_msg_id'])    ? $results['no_answer_msg_id']    : DEFAULT_MSG;
 	$results['invalidnmbr_msg_id']  = isset($results['invalidnmbr_msg_id'])  ? $results['invalidnmbr_msg_id']  : DEFAULT_MSG;
 	return $results;
--- outroutemsg/page.outroutemsg.php
+++ outroutemsg/page.outroutemsg.php
@@ -16,11 +16,12 @@ if($action){
 	$default_msg_id      = isset($_REQUEST['default_msg_id'])      ? trim($_REQUEST['default_msg_id'])      : DEFAULT_MSG;
 	$intracompany_msg_id = isset($_REQUEST['intracompany_msg_id']) ? trim($_REQUEST['intracompany_msg_id']) : DEFAULT_MSG;
 	$emergency_msg_id    = isset($_REQUEST['emergency_msg_id'])    ? trim($_REQUEST['emergency_msg_id'])    : DEFAULT_MSG;
+	$unallocated_msg_id  = isset($_REQUEST['unallocated_msg_id'])  ? trim($_REQUEST['unallocated_msg_id'])  : DEFAULT_MSG;
 	$no_answer_msg_id    = isset($_REQUEST['no_answer_msg_id'])    ? trim($_REQUEST['no_answer_msg_id'])    : DEFAULT_MSG;	
 	$invalidnmbr_msg_id  = isset($_REQUEST['invalidnmbr_msg_id'])  ? trim($_REQUEST['invalidnmbr_msg_id'])  : DEFAULT_MSG;
 
 	if ($action == 'submit') {
-		outroutemsg_add($default_msg_id, $intracompany_msg_id, $emergency_msg_id, $no_answer_msg_id, $invalidnmbr_msg_id);
+		outroutemsg_add($default_msg_id, $intracompany_msg_id, $emergency_msg_id, $no_answer_msg_id, $invalidnmbr_msg_id, $unallocated_msg_id);
 		needreload();
 	}
 }
@@ -33,6 +34,7 @@ if ($action != 'submit') {
 	$default_msg_id      = $outroutemsg_settings['default_msg_id'];
 	$intracompany_msg_id = $outroutemsg_settings['intracompany_msg_id'];
 	$emergency_msg_id    = $outroutemsg_settings['emergency_msg_id'];
+	$unallocated_msg_id  = $outroutemsg_settings['unallocated_msg_id'];
 	$no_answer_msg_id    = $outroutemsg_settings['no_answer_msg_id'];
 	$invalidnmbr_msg_id  = $outroutemsg_settings['invalidnmbr_msg_id'];
 }
@@ -52,6 +54,7 @@ if ($action != 'submit') {
 		<?php
 			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $default_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
 			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $default_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+			echo '<option value="'.INFO_TONE.'"'.(INFO_TONE == $default_msg_id ? ' SELECTED' : '').'>'._("Info Tone")."</option>\n";
 			if (isset($tresults[0])) {
 				foreach ($tresults as $tresult) {
 					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $default_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
@@ -70,6 +73,7 @@ if ($action != 'submit') {
 		<?php
 			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $intracompany_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
 			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $intracompany_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+			echo '<option value="'.INFO_TONE.'"'.(INFO_TONE == $intracompany_msg_id ? ' SELECTED' : '').'>'._("Info Tone")."</option>\n";
 			if (isset($tresults[0])) {
 				foreach ($tresults as $tresult) {
 					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $intracompany_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
@@ -88,6 +92,7 @@ if ($action != 'submit') {
 		<?php
 			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $emergency_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
 			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $emergency_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+			echo '<option value="'.INFO_TONE.'"'.(INFO_TONE == $emergency_msg_id ? ' SELECTED' : '').'>'._("Info Tone")."</option>\n";
 			if (isset($tresults[0])) {
 				foreach ($tresults as $tresult) {
 					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $emergency_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
@@ -97,7 +102,27 @@ if ($action != 'submit') {
 		</select>
 	</td>
 </tr>
+
 <tr><td colspan="2"><br><h4><?php echo _("Trunk Failures")?></h4></td></tr>
+
+<tr><td colspan="2"><h5><?php echo _("Unallocated Numbers")?><hr></h5></td></tr>
+<tr>
+	<td><a href="#" class="info"><?php echo _("Message or Tone")?><span><?php echo _("Message or tone to be played if destination number is unallocated/does not exists.")?></span></a></td>
+	<td align=right>
+		<select name="unallocated_msg_id" id="unallocated_msg_id" tabindex="<?php echo ++$tabindex;?>">
+		<?php
+			echo '<option value="'.DEFAULT_MSG.'"'.(DEFAULT_MSG == $unallocated_msg_id ? ' SELECTED' : '').'>'._("Default Message")."</option>\n";
+			echo '<option value="'.CONGESTION_TONE.'"'.(CONGESTION_TONE == $unallocated_msg_id ? ' SELECTED' : '').'>'._("Congestion Tones")."</option>\n";
+			echo '<option value="'.INFO_TONE.'"'.(INFO_TONE == $unallocated_msg_id ? ' SELECTED' : '').'>'._("Info Tone")."</option>\n";
+			if (isset($tresults[0])) {
+				foreach ($tresults as $tresult) {
+					echo '<option value="'.$tresult['id'].'"'.($tresult['id'] == $unallocated_msg_id ? ' SELECTED' : '').'>'.$tresult['displayname']."</option>\n";
+				}
+			}
+		?>
+		</select>
+	</td>
+</tr>
 
 <tr><td colspan="2"><h5><?php echo _("No Answer")?><hr></h5></td></tr>
 <tr>
