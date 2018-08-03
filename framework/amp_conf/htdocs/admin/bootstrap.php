fee09efdf2b0c54398af4ec375382667c643ebc0
Nicolas Gudino <asternic@gmail.com>
2018-08-03 17:27:28 -0400

    White space and 5 attempts to connect to AMI before fatal error

--- framework/amp_conf/htdocs/admin/bootstrap.php
+++ framework/amp_conf/htdocs/admin/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
-//	License for all code of this IssabelPBX module can be found in the license file inside the module directory
-//	Copyright 2013 Schmooze Com Inc.
+//    License for all code of this IssabelPBX module can be found in the license file inside the module directory
+//    Copyright 2018 Issabel Foundation
+//    Copyright 2013 Schmooze Com Inc.
 //
 
 /* 
@@ -39,7 +40,7 @@ $benchmark_starttime = microtime_float();
 
 global $amp_conf;
 if (empty($amp_conf['AMPWEBROOT'])) {
-	$amp_conf['AMPWEBROOT'] = dirname(dirname(__FILE__));
+    $amp_conf['AMPWEBROOT'] = dirname(dirname(__FILE__));
 }
 $dirname = $amp_conf['AMPWEBROOT'] . '/admin';
 
@@ -64,13 +65,13 @@ $bootstrap_settings['cdrdb'] = isset($bootstrap_settings['cdrdb']) ? $bootstrap_
 
 $restrict_mods = isset($restrict_mods) ? $restrict_mods : false;
 
-	 	 
+          
 // include base functions
 require_once($dirname . '/libraries/utility.functions.php'); 
 $bootstrap_settings['framework_functions_included'] = false; 
 require_once($dirname . '/functions.inc.php'); 
 $bootstrap_settings['framework_functions_included'] = true; 
-	 	 
+          
 //now that its been included, use our own error handler as it tends to be much more verbose. 
 if ($bootstrap_settings['issabelpbx_error_handler']) {
   $error_handler = $bootstrap_settings['issabelpbx_error_handler'] === true ? 'issabelpbx_error_handler' : $bootstrap_settings['issabelpbx_error_handler'];
@@ -97,35 +98,44 @@ $bootstrap_settings['amportal_conf_initialized'] = true;
 
 //connect to cdrdb if requestes
 if ($bootstrap_settings['cdrdb']) {
-	$dsn = array(
-		'phptype'  => $amp_conf['CDRDBTYPE'] ? $amp_conf['CDRDBTYPE'] : $amp_conf['AMPDBENGINE'],
-		'hostspec' => $amp_conf['CDRDBHOST'] ? $amp_conf['CDRDBHOST'] : $amp_conf['AMPDBHOST'],
-		'username' => $amp_conf['CDRDBUSER'] ? $amp_conf['CDRDBUSER'] : $amp_conf['AMPDBUSER'],
-		'password' => $amp_conf['CDRDBPASS'] ? $amp_conf['CDRDBPASS'] : $amp_conf['AMPDBPASS'],
-		'port'     => $amp_conf['CDRDBPORT'] ? $amp_conf['CDRDBPORT'] : '3306',
-		//'socket'   => $amp_conf['CDRDBTYPE'] ? $amp_conf['CDRDBTYPE'] : 'mysql',
-		'database' => $amp_conf['CDRDBNAME'] ? $amp_conf['CDRDBNAME'] : 'asteriskcdrdb',
-	);
-	$cdrdb = DB::connect($dsn);
+    $dsn = array(
+        'phptype'  => $amp_conf['CDRDBTYPE'] ? $amp_conf['CDRDBTYPE'] : $amp_conf['AMPDBENGINE'],
+        'hostspec' => $amp_conf['CDRDBHOST'] ? $amp_conf['CDRDBHOST'] : $amp_conf['AMPDBHOST'],
+        'username' => $amp_conf['CDRDBUSER'] ? $amp_conf['CDRDBUSER'] : $amp_conf['AMPDBUSER'],
+        'password' => $amp_conf['CDRDBPASS'] ? $amp_conf['CDRDBPASS'] : $amp_conf['AMPDBPASS'],
+        'port'     => $amp_conf['CDRDBPORT'] ? $amp_conf['CDRDBPORT'] : '3306',
+        //'socket'   => $amp_conf['CDRDBTYPE'] ? $amp_conf['CDRDBTYPE'] : 'mysql',
+        'database' => $amp_conf['CDRDBNAME'] ? $amp_conf['CDRDBNAME'] : 'asteriskcdrdb',
+    );
+    $cdrdb = DB::connect($dsn);
 }
 
 $bootstrap_settings['astman_connected'] = false;
 if (!$bootstrap_settings['skip_astman']) {
-	require_once($dirname . '/libraries/php-asmanager.php');
-  $astman	= new AGI_AsteriskManager($bootstrap_settings['astman_config'], $bootstrap_settings['astman_options']);
-	// attempt to connect to asterisk manager proxy
-	if (!$amp_conf["ASTMANAGERPROXYPORT"] || !$res = $astman->connect($amp_conf["ASTMANAGERHOST"] . ":" . $amp_conf["ASTMANAGERPROXYPORT"], $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"], $bootstrap_settings['astman_events'])) {
-		// attempt to connect directly to asterisk, if no proxy or if proxy failed
-		if (!$res = $astman->connect($amp_conf["ASTMANAGERHOST"] . ":" . $amp_conf["ASTMANAGERPORT"], $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"], $bootstrap_settings['astman_events'])) {
-			// couldn't connect at all
-			unset( $astman );
-			issabelpbx_log(FPBX_LOG_CRITICAL,"Connection attmempt to AMI failed");
-		} else {
-			$bootstrap_settings['astman_connected'] = true;
-		}
-	}
+    require_once($dirname . '/libraries/php-asmanager.php');
+    $astman = new AGI_AsteriskManager($bootstrap_settings['astman_config'], $bootstrap_settings['astman_options']);
+    // attempt to connect to asterisk manager proxy
+    if (!$amp_conf["ASTMANAGERPROXYPORT"] || !$res = $astman->connect($amp_conf["ASTMANAGERHOST"] . ":" . $amp_conf["ASTMANAGERPROXYPORT"], $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"], $bootstrap_settings['astman_events'])) {
+        // attempt to connect directly to asterisk, if no proxy or if proxy failed
+        $i=0;
+        do {
+            $i++; 
+            if (!$res = $astman->connect($amp_conf["ASTMANAGERHOST"] . ":" . $amp_conf["ASTMANAGERPORT"], $amp_conf["AMPMGRUSER"] , $amp_conf["AMPMGRPASS"], $bootstrap_settings['astman_events'])) {
+                // couldn't connect at all
+                sleep(2);
+            } else {
+                $bootstrap_settings['astman_connected'] = true;
+                $i=10;
+            }
+        } while ($i < 5);
+
+        if($i<10) {
+            unset( $astman );
+            issabelpbx_log(FPBX_LOG_CRITICAL,"Connection attmempt to AMI failed");
+        }
+    }
 } else {
-	$bootstrap_settings['astman_connected'] = true;
+    $bootstrap_settings['astman_connected'] = true;
 }
 
 //include gui functions + auth if nesesarry
@@ -135,13 +145,13 @@ if (!$bootstrap_settings['skip_astman']) {
 //       if we want to bypass authentication and not try to be automatic about it?
 //
 if (!$bootstrap_settings['issabelpbx_auth'] || (php_sapi_name() == 'cli')) {
-	if (!defined('ISSABELPBX_IS_AUTH')) {
-		define('ISSABELPBX_IS_AUTH', 'TRUE');
-		define('FREEPBX_IS_AUTH', 'TRUE');
-	}
+    if (!defined('ISSABELPBX_IS_AUTH')) {
+        define('ISSABELPBX_IS_AUTH', 'TRUE');
+        define('FREEPBX_IS_AUTH', 'TRUE');
+    }
 } else {
-	require($dirname . '/libraries/gui_auth.php');
-	frameworkPasswordCheck();
+    require($dirname . '/libraries/gui_auth.php');
+    frameworkPasswordCheck();
 }
 if (!isset($no_auth) && !defined('ISSABELPBX_IS_AUTH')) { die('No direct script access allowed'); }//we should never need this, just another line of defence
 bootstrap_include_hooks('pre_module_load', 'all_mods');
@@ -152,65 +162,65 @@ $restrict_mods_local = $restrict_mods;
 //the module info as we are going down a path such as an ajax path that this 
 //is just overhead. (We'll know soon enough if this is too restrcitive).
 if ($restrict_mods_local !== true) {
-	$isauth = !isset($no_auth);
-	$active_modules = module_getinfo(false, MODULE_STATUS_ENABLED);
-	$modpath = $amp_conf['AMPWEBROOT'] . '/admin/modules/';
+    $isauth = !isset($no_auth);
+    $active_modules = module_getinfo(false, MODULE_STATUS_ENABLED);
+    $modpath = $amp_conf['AMPWEBROOT'] . '/admin/modules/';
 
-	if(is_array($active_modules)){
-		$force_autoload = false;
-		foreach($active_modules as $key => $module) {
-			//check if this module was was excluded
-			$is_selected = is_array($restrict_mods_local) 
-				&& isset($restrict_mods_local[$key]);
+    if(is_array($active_modules)){
+        $force_autoload = false;
+        foreach($active_modules as $key => $module) {
+            //check if this module was was excluded
+            $is_selected = is_array($restrict_mods_local) 
+                && isset($restrict_mods_local[$key]);
 
-			//get file path
-			$file = $modpath . $key .'/functions.inc.php';
-			$file_exists = is_file($file);
-			
-			//check authentication, skip this module if we dont have auth
-			$needs_auth = isset($module['requires_auth']) 
-				&& $module['requires_auth'] == 'false'
-				? false : true;
-			if (!$isauth && $needs_auth) {
-				continue;
-			}
+            //get file path
+            $file = $modpath . $key .'/functions.inc.php';
+            $file_exists = is_file($file);
+            
+            //check authentication, skip this module if we dont have auth
+            $needs_auth = isset($module['requires_auth']) 
+                && $module['requires_auth'] == 'false'
+                ? false : true;
+            if (!$isauth && $needs_auth) {
+                continue;
+            }
 
-			// Zend appears to break class auto-loading. Therefore, if we 
-			//detect there is a module that requires Zend 
-			// we will include all the potential classes at this point.
-			$needs_zend = isset($module['depends']['phpcomponent']) 
-				&& stristr($module['depends']['phpcomponent'], 'zend');
-			if (!$force_autoload && $needs_zend) {
-				fpbx_framework_autoloader(true);
-				$force_autoload = true;
-			}
+            // Zend appears to break class auto-loading. Therefore, if we 
+            //detect there is a module that requires Zend 
+            // we will include all the potential classes at this point.
+            $needs_zend = isset($module['depends']['phpcomponent']) 
+                && stristr($module['depends']['phpcomponent'], 'zend');
+            if (!$force_autoload && $needs_zend) {
+                fpbx_framework_autoloader(true);
+                $force_autoload = true;
+            }
 
-			//actualy load module
-			if ((!$restrict_mods_local || $is_selected) && $file_exists) {
-				bootstrap_include_hooks('pre_module_load', $key);
-				require_once($file);
-				bootstrap_include_hooks('post_module_load', $key);
-			} 
-			//create an array of module sections to display
-			//stored as [items][$type][$category][$name] = $displayvalue
-			if (isset($module['items']) && is_array($module['items'])) {
-				//if asterisk isnt running, mark moduels that depend on 
-				//asterisk as disbaled
-				foreach($module['items'] as $itemKey => $item) {
-					$needs_edb = isset($item['needsenginedb']) 
-							&& strtolower($item['needsenginedb']) == 'yes';
-					$needs_running = isset($item['needsenginerunning']) 
-							&& strtolower($item['needsenginerunning']) == 'yes';
-					$needs_astman = $needs_edb || $needs_running;
-					if ((!isset($astman) || !$astman) && $needs_astman) {
-						$active_modules[$key]['items'][$itemKey]['disabled'] 
-							= true;
-					}
-				}
-			}
-		}
-	}
-	bootstrap_include_hooks('post_module_load', 'all_mods');
-	$bootstrap_settings['function_modules_included'] = true;
+            //actualy load module
+            if ((!$restrict_mods_local || $is_selected) && $file_exists) {
+                bootstrap_include_hooks('pre_module_load', $key);
+                require_once($file);
+                bootstrap_include_hooks('post_module_load', $key);
+            } 
+            //create an array of module sections to display
+            //stored as [items][$type][$category][$name] = $displayvalue
+            if (isset($module['items']) && is_array($module['items'])) {
+                //if asterisk isnt running, mark moduels that depend on 
+                //asterisk as disbaled
+                foreach($module['items'] as $itemKey => $item) {
+                    $needs_edb = isset($item['needsenginedb']) 
+                            && strtolower($item['needsenginedb']) == 'yes';
+                    $needs_running = isset($item['needsenginerunning']) 
+                            && strtolower($item['needsenginerunning']) == 'yes';
+                    $needs_astman = $needs_edb || $needs_running;
+                    if ((!isset($astman) || !$astman) && $needs_astman) {
+                        $active_modules[$key]['items'][$itemKey]['disabled'] 
+                            = true;
+                    }
+                }
+            }
+        }
+    }
+    bootstrap_include_hooks('post_module_load', 'all_mods');
+    $bootstrap_settings['function_modules_included'] = true;
 }
 ?>
