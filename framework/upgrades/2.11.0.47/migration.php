594ee50a15effdafc25aac3fe8f44472f5dd519e
Nicolas Gudino <asternic@gmail.com>
2018-09-06 22:57:08 -0400

    Upgrade jquery-ui version on settings

--- framework/upgrades/2.11.0.47/migration.php
+++ framework/upgrades/2.11.0.47/migration.php
@@ -1,2 +1,5 @@
 <?php
-//do nothing
+global $amp_conf;
+include_once ($amp_conf['AMPWEBROOT'].'/admin/libraries/issabelpbx_conf.class.php');
+$issabelpbx_conf =& issabelpbx_conf::create();
+$issabelpbx_conf->set_conf_values(array('JQUERYUI_VER' => '1.9.1'),true);
--- framework/upgrades/2.11.0.47/update_release_46.sql
+++
@@ -1,2 +0,0 @@
-UPDATE issabelpbx_settings SET value='1.9.1' WHERE keyword='JQUERYUI_VER';
-
