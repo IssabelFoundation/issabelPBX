eabad67d06cf00e547d38f87c99b3249b0dc1c6b
Nicolas Gudino <asternic@gmail.com>
2019-11-04 08:45:13 -0500

    Update tranlation

---
+++ dynroute/titlelang.php
@@ -0,0 +1,8 @@
+<?php
+$modTitle=array();
+$modTitle['es']['Dynamic Routes'] = 'Rutas Dinámicas';
+$modTitle['br']['Dynamic Routes'] = 'Rotas Dinâmicas';
+$modTitle['fr']['Dynamic Routes'] = "Routes Dynamiques";
+
+
+
