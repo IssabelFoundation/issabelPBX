432970acea901d05aefbc3bf68db85916f651e7e
Nicolas Gudino <asternic@gmail.com>
2021-05-16 17:50:16 -0400

    Update spanish translation

--- parking/titlelang.php
+++ parking/titlelang.php
@@ -1,6 +1,6 @@
 <?php
 $modTitle=array();
-$modTitle['es']['Parking'] = 'Aparcamiento';
+$modTitle['es']['Parking'] = 'Parqueo';
 $modTitle['br']['Parking'] = 'Estacionamiento';
 $modTitle['fr']['Parking'] = "Parcage d'appel";
 
