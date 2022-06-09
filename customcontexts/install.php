cd01ffd8ab0efa97a35c6c291f15921654377b79
Nicolás <nicolas@issabel.com>
2022-06-09 12:37:40 -0400

    Make table create and update on customcontext work with sqlite/rqlite

--- customcontexts/install.php
+++ customcontexts/install.php
@@ -36,18 +36,16 @@ if (!function_exists('timeconditions_timegroups_add_group_timestrings')) {
 
 $sql[] ="CREATE TABLE IF NOT EXISTS `customcontexts_contexts` (
 				`context` varchar(100) NOT NULL default '',
-				`description` varchar(100) NOT NULL default '',
-				PRIMARY KEY  (`context`),
-				UNIQUE KEY `description` (`description`)
+				`description` varchar(100) NOT NULL UNIQUE default '',
+				PRIMARY KEY  (`context`)
 			)";
 
 
 $sql[] ="CREATE TABLE IF NOT EXISTS `customcontexts_contexts_list` (
 				`context` varchar(100) NOT NULL default '',
-				`description` varchar(100) NOT NULL default '',
+				`description` varchar(100) NOT NULL UNIQUE default '',
 				`locked` tinyint(1) NOT NULL default '0',
-				PRIMARY KEY  (`context`),
-				UNIQUE KEY `description` (`description`)
+				PRIMARY KEY  (`context`)
 				)";
 
 $sql[] ="INSERT IGNORE INTO `customcontexts_contexts_list` 
@@ -61,8 +59,7 @@ $sql[] ="CREATE TABLE IF NOT EXISTS `customcontexts_includes` (
 				`include` varchar(100) NOT NULL default '',
 				`timegroupid` int(11) default NULL,
 				`sort` int(11) NOT NULL default '0',
-				PRIMARY KEY  (`context`,`include`),
-				KEY `sort` (`sort`)
+				PRIMARY KEY  (`context`,`include`)
 )";
 
 $sql[] ="ALTER IGNORE TABLE `customcontexts_includes` ADD `timegroupid` INT NULL AFTER `include`";
@@ -100,33 +97,49 @@ $sql[] ="INSERT IGNORE INTO `customcontexts_module` (`id`, `value`) VALUES ('mod
 
 $sql[] ="UPDATE `customcontexts_module` set `value` = '0.3.2' where `id` = 'moduleversion';";
 
-foreach ($sql as $q){
+foreach ($sql as $dengine=>$q){
 	$db->query($q);
 		if(DB::IsError($q)) { 
 			out("FATAL: ".$q->getDebugInfo()."\n");	
 		}
 }
 
+if($amp_conf['AMBDBENGINE']=='mysql' || $amp_conf['AMPDBENGINE']=='mysqli') {
+    $sql = "ALTER TABLE customcontexts_includes ADD index sort(sort)";
+} else {
+    // for sqlite3/rqlite
+    $sql = "CREATE INDEX sort ON customcontexts_includes(sort)";
+}
+$db->query($sql);
+
 customcontexts_updatedb();
 
 //bring db up to date on install/upgrade
 function customcontexts_updatedb() {
 	global $db;
-	$sql = "ALTER IGNORE TABLE `customcontexts_includes` ADD `timegroupid` INT NULL AFTER `include` ;";
+	$sql = "ALTER TABLE `customcontexts_includes` ADD `timegroupid` INT NULL AFTER `include` ;";
 	$db->query($sql);
-	$sql = "ALTER IGNORE TABLE `customcontexts_includes_list` ADD `missing` BOOL NOT NULL DEFAULT '0';";
+	$sql = "ALTER TABLE `customcontexts_includes_list` ADD `missing` BOOL NOT NULL DEFAULT '0';";
 	$db->query($sql);
-	$sql = "ALTER IGNORE TABLE `customcontexts_contexts` ADD `dialrules` VARCHAR( 1000 ) NULL;";
+	$sql = "ALTER TABLE `customcontexts_contexts` ADD `dialrules` VARCHAR( 1000 ) NULL;";
 	$db->query($sql);
-	$sql = "ALTER IGNORE TABLE `customcontexts_includes` ADD `userules` VARCHAR( 10 ) NULL ;";
+	$sql = "ALTER TABLE `customcontexts_includes` ADD `userules` VARCHAR( 10 ) NULL ;";
 	$db->query($sql);
-	$sql = "ALTER IGNORE TABLE `customcontexts_contexts` ADD `faildestination` VARCHAR( 250 ) NULL , ADD `featurefaildestination` VARCHAR( 250 ) NULL ;";
+	$sql = "ALTER TABLE `customcontexts_contexts` ADD `faildestination` VARCHAR( 250 ) NULL ;";
+	$db->query($sql);
+	$sql = "ALTER TABLE `customcontexts_contexts` ADD `featurefaildestination` VARCHAR( 250 ) NULL ;";
 	$db->query($sql);
 //0.3.0
-	$sql = "ALTER IGNORE TABLE `customcontexts_contexts` ADD `failpin` VARCHAR( 100 ) NULL , ADD `failpincdr` BOOL NOT NULL DEFAULT '0', ADD `featurefailpin` VARCHAR( 100 ) NULL , ADD `featurefailpincdr` BOOL NOT NULL DEFAULT '0';";
+	$sql = "ALTER TABLE `customcontexts_contexts` ADD `failpin` VARCHAR( 100 ) NULL ;";
+	$db->query($sql);
+    $sql = "ALTER TABLE `customcontexts_contexts` ADD `failpincdr` BOOL NOT NULL DEFAULT '0' ;";
+	$db->query($sql);
+	$sql = "ALTER TABLE `customcontexts_contexts` ADD `featurefailpin` VARCHAR( 100 ) NULL ;";
+	$db->query($sql);
+	$sql = "ALTER TABLE `customcontexts_contexts` ADD `featurefailpincdr` BOOL NOT NULL DEFAULT '0';";
 	$db->query($sql);
 //0.3.2
-	$sql = "ALTER IGNORE TABLE `customcontexts_includes_list` ADD `sort` INT NOT NULL DEFAULT '0';";
+	$sql = "ALTER TABLE `customcontexts_includes_list` ADD `sort` INT NOT NULL DEFAULT '0';";
 	$db->query($sql);
 }
 
