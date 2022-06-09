0e00913cf6e22345a4072a724a70b050bbf03857
Nicolás <nicolas@issabel.com>
2022-06-09 12:36:11 -0400

    Make proper CREATE table for sqlite/rqlite

--- dynamicfeatures/install.php
+++ dynamicfeatures/install.php
@@ -3,17 +3,33 @@ if (!defined('ISSABELPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
 global $db;
 
-$autoincrement = (($amp_conf["AMPDBENGINE"] == "sqlite") || ($amp_conf["AMPDBENGINE"] == "sqlite3")) ? "AUTOINCREMENT":"AUTO_INCREMENT";
-$sql[]="CREATE TABLE IF NOT EXISTS `dynamicfeatures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `dtmf` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
-  `activate_on` enum('self','peer') COLLATE utf8mb4_unicode_ci DEFAULT 'peer',
-  `application` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT '',
-  `arguments` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT '',
-  `moh_class` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT '',
-  PRIMARY KEY (`id`)
-) ";
+if($amp_conf["AMPDBENGINE"]=="mysql" || $amp_conf["AMPDBENGINE"]=="mysqli") {
+
+    $sql[]="CREATE TABLE IF NOT EXISTS `dynamicfeatures` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(200) NOT NULL DEFAULT '',
+        `dtmf` varchar(10) NOT NULL DEFAULT '',
+        `activate_on` enum('self','peer') DEFAULT 'peer',
+        `application` varchar(100) DEFAULT '',
+        `arguments` varchar(200) DEFAULT '',
+        `moh_class` varchar(200) DEFAULT '',
+        PRIMARY KEY (`id`)
+            ) ";
+
+} else {
+
+    $sql[]="CREATE TABLE IF NOT EXISTS `dynamicfeatures` (
+        `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+        `name` varchar(200) NOT NULL DEFAULT '',
+        `dtmf` varchar(10) NOT NULL DEFAULT '',
+        `activate_on` TEXT CHECK( activate_on IN ('self','peer')) DEFAULT 'peer',
+        `application` varchar(100) DEFAULT '',
+        `arguments` varchar(200) DEFAULT '',
+        `moh_class` varchar(200) DEFAULT ''
+            ) ";
+
+
+}
 
 foreach($sql as $s){
     $check = $db->query($s);
