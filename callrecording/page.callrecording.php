922c435aa567ef49c0270945c86fbd468f321646
Nicolás <nicolas@issabel.com>
2022-07-15 22:34:18 -0400

    Fix current class for selected items in rnav

--- callrecording/page.callrecording.php
+++ callrecording/page.callrecording.php
@@ -38,10 +38,10 @@ switch ($action) {
 <div class="rnav"><ul>
 <?php 
 
-echo '<li><a href="config.php?display=callrecording&amp;type='.$type.'">'._('Add Call Recording').'</a></li>';
+echo '<li><a class="'.($extdisplay=='' ? 'current':'').'" href="config.php?display=callrecording&amp;type='.$type.'">'._('Add Call Recording').'</a></li>';
 
 foreach (callrecording_list() as $row) {
-	echo '<li><a href="config.php?display=callrecording&amp;type='.$type.'&amp;extdisplay='.$row['callrecording_id'].'" class="">'.$row['description'].'</a></li>';
+	echo '<li><a class="'.($extdisplay==$row['callrecording_id'] ? 'current':'').'" href="config.php?display=callrecording&amp;type='.$type.'&amp;extdisplay='.$row['callrecording_id'].'" class="">'.$row['description'].'</a></li>';
 }
 
 ?>
