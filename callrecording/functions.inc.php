7b246c818014b2947a6314cbfe8d7dc668ec8fd6
Nicolás Gudiño <nicolas@house.com.ar>
2022-08-19 19:29:53 -0300

    Update callrecording module to 2.12.0.0 for new GUI/CSS/JS

--- callrecording/functions.inc.php
+++ callrecording/functions.inc.php
@@ -217,6 +217,7 @@ function callrecording_edit($callrecording_id, $description, $callrecording_mode
 }
 
 function callrecording_hook_core($viewing_itemid, $target_menuid){
+    global $tabindex;
 
 	switch ($target_menuid) {
 	case 'did':
@@ -255,19 +256,22 @@ function callrecording_hook_core($viewing_itemid, $target_menuid){
 	break;
 	}
 	$html = '';
-	//if ($target_menuid == 'did'){
 	if ($target_menuid == 'did' || $target_menuid == 'routing') {
-    global $tabindex;
+        global $tabindex;
 		if ($target_menuid == 'did') {
 			$html.='<tr><td colspan="2"><h5>'._("Call Recording").'</h5></td></tr>';
 		}
-		$html.='<tr><td><a href="#" class="info">'._('Call Recording').'<span>'._("Controls or overrides the call recording behavior for calls coming into this DID. Allow will honor the normal downstream call recording settings. Record on Answer starts recording when the call would otherwise be recorded ignoring any settings that say otherwise. Record Immediately will start recording right away capturing ringing, announcements, MoH, etc. Never will disallow recording regardless of downstream settings.").'</span></a></td>';
-		$html.='<td><select name="callrecording" tabindex="' . ++$tabindex . '" class="componentSelect">'."\n";
-    $html.= '<option value=""' . ($callrecording == ''  ? ' SELECTED' : '').'>'._("Allow")."\n";
-    $html.= '<option value="delayed"'. ($callrecording == 'delayed' ? ' SELECTED' : '').'>'._("Record on Answer")."\n";
-    $html.= '<option value="force"'  . ($callrecording == 'force'   ? ' SELECTED' : '').'>'._("Record Immediately")."\n";
-    $html.= '<option value="never"' . ($callrecording == 'never'  ? ' SELECTED' : '').'>'._("Never")."\n";
-    $html.= "</select></td></tr>\n";
+        $html.='<tr><td><a href="#" class="info">'._('Call Recording').'<span>'._("Controls or overrides the call recording behavior for calls coming into this DID. Allow will honor the normal downstream call recording settings. Record on Answer starts recording when the call would otherwise be recorded ignoring any settings that say otherwise. Record Immediately will start recording right away capturing ringing, announcements, MoH, etc. Never will disallow recording regardless of downstream settings.").'</span></a></td>';
+        if($tabindex=='') {
+		    $html.='<td><select name="callrecording" tabindex="{tabindex}" class="componentSelect">'."\n";
+        } else {
+		    $html.='<td><select name="callrecording" tabindex="' . ++$tabindex . '" class="componentSelect">'."\n";
+        }
+        $html.= '<option value=""' . ($callrecording == ''  ? ' SELECTED' : '').'>'._("Allow")."\n";
+        $html.= '<option value="delayed"'. ($callrecording == 'delayed' ? ' SELECTED' : '').'>'._("Record on Answer")."\n";
+        $html.= '<option value="force"'  . ($callrecording == 'force'   ? ' SELECTED' : '').'>'._("Record Immediately")."\n";
+        $html.= '<option value="never"' . ($callrecording == 'never'  ? ' SELECTED' : '').'>'._("Never")."\n";
+        $html.= "</select></td></tr>\n";
 	}
 	return $html;
 }
--- callrecording/page.callrecording.php
+++ callrecording/page.callrecording.php
@@ -1,88 +1,94 @@
 <?php 
 if (!defined('ISSABELPBX_IS_AUTH')) { die('No direct script access allowed'); }
+//  License for all code of this IssabelPBX module can be found in the license file inside the module directory
+//  Copyright 2022 Issabel Foundation
 
 $tabindex = 0;
-$type = isset($_REQUEST['type']) ? $_REQUEST['type'] : 'setup';
-$action = isset($_REQUEST['action']) ? $_REQUEST['action'] :  '';
+$type               = isset($_REQUEST['type'])               ? $_REQUEST['type']               :'setup';
+$action             = isset($_REQUEST['action'])             ? $_REQUEST['action']             : '';
+$callrecording_id   = isset($_REQUEST['callrecording_id'])   ? $_REQUEST['callrecording_id']   : false;
+$description        = isset($_REQUEST['description'])        ? $_REQUEST['description']        : '';
+$callrecording_mode = isset($_REQUEST['callrecording_mode']) ? $_REQUEST['callrecording_mode'] : '';
+$dest               = isset($_REQUEST['dest'])               ? $_REQUEST['dest']               : '';
+
 if (isset($_REQUEST['delete'])) $action = 'delete'; 
 
-$callrecording_id = isset($_REQUEST['callrecording_id']) ? $_REQUEST['callrecording_id'] :  false;
-$description = isset($_REQUEST['description']) ? $_REQUEST['description'] :  '';
-$callrecording_mode = isset($_REQUEST['callrecording_mode']) ? $_REQUEST['callrecording_mode'] :  '';
-$dest = isset($_REQUEST['dest']) ? $_REQUEST['dest'] :  '';
-
 if (isset($_REQUEST['goto0']) && $_REQUEST['goto0']) {
-	$dest = $_REQUEST[ $_REQUEST['goto0'].'0' ];
+	$dest = $_REQUEST[ $_REQUEST['goto0'] ];
 }
 
 switch ($action) {
 	case 'add':
 		$_REQUEST['extdisplay'] = callrecording_add($description, $callrecording_mode, $dest);
-		needreload();
-		redirect_standard('extdisplay');
+        needreload();
+        $_SESSION['msg']=base64_encode(dgettext('amp','Item has been added'));
+        $_SESSION['msgtype']='success';
+		redirect_standard();
 	break;
 	case 'edit':
 		callrecording_edit($callrecording_id, $description, $callrecording_mode, $dest);
-		needreload();
+        needreload();
+        $_SESSION['msg']=base64_encode(dgettext('amp','Item has been saved'));
+        $_SESSION['msgtype']='success';
 		redirect_standard('extdisplay');
 	break;
 	case 'delete':
 		callrecording_delete($callrecording_id);
-		needreload();
+        needreload();
+        $_SESSION['msg']=base64_encode(dgettext('amp','Item has been deleted'));
+        $_SESSION['msgtype']='warning';
 		redirect_standard();
 	break;
 }
 
-?> 
-
-<div class="rnav"><ul>
-<?php 
-
-echo '<li><a class="'.($extdisplay=='' ? 'current':'').'" href="config.php?display=callrecording&amp;type='.$type.'">'._('Add Call Recording').'</a></li>';
-
-foreach (callrecording_list() as $row) {
-	echo '<li><a class="'.($extdisplay==$row['callrecording_id'] ? 'current':'').'" href="config.php?display=callrecording&amp;type='.$type.'&amp;extdisplay='.$row['callrecording_id'].'" class="">'.$row['description'].'</a></li>';
+$rnavitems = array();
+$callrec   = callrecording_list();
+foreach ($callrec as $row) {
+    $rnavitems[]=array($row['callrecording_id'],$row['description'],'','');
 }
-
+drawListMenu($rnavitems, $type, $display, $extdisplay);
 ?>
-</ul></div>
-
+<div class='content'>
 <?php
-
 if ($extdisplay) {
 	// load
 	$row = callrecording_get($extdisplay);
 	
-	$description = $row['description'];
-	$callrecording_mode   = $row['callrecording_mode'];
-	$dest        = $row['dest'];
-
+	$description        = $row['description'];
+	$callrecording_mode = $row['callrecording_mode'];
+	$dest               = $row['dest'];
 	$cm_disp = $callrecording_mode ? $callrecording_mode : 'allow';
-	echo "<h2>"._("Edit: ")."$description ($cm_disp)"."</h2>";
-} else {
-	echo "<h2>"._("Add Call Recording")."</h2>";
 }
 
 $helptext = _("Call Recordings provide the ability to force a call to be recorded or not recorded based on a call flow and override all other recording settings. If a call is to be recorded, it can start immediately which will incorporate any announcements, hold music, etc. prior to being answered, or it can have recording start at the time that call is answered.");
-echo $helptext;
+$help = '<div class="infohelp">?<span style="display:none;">'.$helptext.'</span></div>';
+
+echo "<div class='is-flex'><h2>".($extdisplay ? _('Edit Call Recording').': '.$description : _("Add Call Recording"))."</h2>$help</div>\n";
+
+if ($extdisplay) {
+    $usage_list = framework_display_destination_usage(callrecording_getdest($extdisplay));
+    if (!empty($usage_list)) {
+        echo ipbx_usage_info($usage_list['text'],$usage_list['tooltip']);
+    }
+}
 ?>
 
-<form name="editCallRecording" action="<?php  $_SERVER['PHP_SELF'] ?>" method="post" onsubmit="return checkCallRecording(editCallRecording);">
+<form id="mainform" name="editCallRecording" action="<?php  $_SERVER['PHP_SELF'] ?>" method="post" onsubmit="return checkCallRecording(this);">
 	<input type="hidden" name="extdisplay" value="<?php echo $extdisplay; ?>">
 	<input type="hidden" name="callrecording_id" value="<?php echo $extdisplay; ?>">
-	<input type="hidden" name="action" value="<?php echo ($extdisplay ? 'edit' : 'add'); ?>">
-	<table>
-	<tr><td colspan="2"><h5><?php  echo ($extdisplay ? _("Edit Call Recording Instance") : _("Add Call Recording Instance")) ?></h5></td></tr>
+    <input type="hidden" name="action" value="<?php echo ($extdisplay ? 'edit' : 'add'); ?>">
+    <table class='table is-borderless is-narrow'>
+    <tr><td colspan="2"><h5><?php echo dgettext('amp','General Settings');?></h5></td></tr>
 	<tr>
 		<td><a href="#" class="info"><?php echo _("Description")?>:<span><?php echo _("The descriptive name of this call recording instance. For example \"French Main IVR\"")?></span></a></td>
-		<td><input size="30" type="text" name="description" value="<?php  echo $description; ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+		<td><input autofocus class="input w100" type="text" name="description" value="<?php  echo $description; ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
 
 
 	<tr>
     <td><a href="#" class="info"><?php echo _("Call Recording Mode")?>:<span><?php echo _("Controls or overrides the call recording behavior for calls continuing through this call flow. Allow will honor the normal downstream call recording settings. Record on Answer starts recording when the call would otherwise be recorded ignoring any settings that say otherwise. Record Immediately will start recording right away capturing ringing, announcements, MoH, etc. Never will disallow recording regardless of downstream settings.")?></span></a></td>
 <?php
-		$callrecording_html = '<td><select class="componentSelect" name="callrecording_mode" tabindex="' . ++$tabindex . '">'."\n";
+	$callrecording_html = '<td><select class="componentSelect" name="callrecording_mode" tabindex="' . ++$tabindex . '">'."\n";
     $callrecording_html.= '<option value=""' . ($callrecording_mode == ''  ? ' SELECTED' : '').'>'._("Allow")."\n";
     $callrecording_html.= '<option value="delayed"'. ($callrecording_mode == 'delayed' ? ' SELECTED' : '').'>'._("Record on Answer")."\n";
     $callrecording_html.= '<option value="force"'  . ($callrecording_mode == 'force'   ? ' SELECTED' : '').'>'._("Record Immediately")."\n";
@@ -90,36 +96,17 @@ echo $helptext;
     $callrecording_html.= "</select></td></tr>\n";
     echo $callrecording_html;
 ?>
-	<tr><td colspan="2"><br><h5><?php echo _("Destination")?>:</h5></td></tr>
+	<tr><td colspan="2"><br><h5><?php echo _("Destination")?></h5></td></tr>
 
 <?php 
 //draw goto selects
 echo drawselects($dest,0);
 ?>
 			
-	<tr>
-		<td colspan="2"><br><input name="Submit" type="submit" value="<?php echo _("Submit Changes")?>" tabindex="<?php echo ++$tabindex;?>">
-			<?php if ($extdisplay) { echo '&nbsp;<input name="delete" type="submit" value="'._("Delete").'">'; } ?>
-		</td>		
-
-		<?php
-		if ($extdisplay) {
-			$usage_list = framework_display_destination_usage(callrecording_getdest($extdisplay));
-			if (!empty($usage_list)) {
-			?>
-				<tr><td colspan="2">
-				<a href="#" class="info"><?php echo $usage_list['text']?>:<span><?php echo $usage_list['tooltip']?></span></a>
-				</td></tr>
-			<?php
-			}
-		}
-		?>
-	</tr>
 </table>
 </form>
 
-<script language="javascript">
-<!--
+<script>
 
 function checkCallRecording(theForm) {
 	var msgInvalidDescription = "<?php echo _('Invalid description specified'); ?>";
@@ -135,7 +122,10 @@ function checkCallRecording(theForm) {
 	if (!validateDestinations(theForm, 1, true))
 		return false;
 
+    $.LoadingOverlay('show');
 	return true;
 }
-//-->
+<?php echo js_display_confirmation_toasts(); ?>
 </script>
+</div> <!-- end div content, be sure to include script tags before -->
+<?php echo form_action_bar($extdisplay); ?>
