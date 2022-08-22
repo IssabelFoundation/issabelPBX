8ea16b95e11d80adf08d0e07ecf094b3b7e8d26c
Nicolás Gudiño <nicolas@house.com.ar>
2022-08-22 18:55:16 -0300

    Update pinesets  module to 2.12.0.0 for new GUI/CSS/JS

--- pinsets/functions.inc.php
+++ pinsets/functions.inc.php
@@ -239,9 +239,9 @@ function pinsets_hook_core($viewing_itemid, $target_menuid) {
 
 			$hookhtml = '
         <tr>
-          <td><a href="#" class="info">'._("PIN Set").'<span>'._('Optional: Select a PIN set to use. If using this option, leave the Route Password field blank.').'</span></a>:</td>
+          <td><a href="#" class="info">'._("PIN Set").'<span>'._('Optional: Select a PIN set to use. If using this option, leave the Route Password field blank.').'</span></a></td>
           <td>
-            <select name="pinsets" class="componentSelect">
+            <select name="pinsets" class="componentSelect" tabindex="{tabindex}">
               <option value="">'._('None').'</option>
       ';
       if (is_array($pinsets)) {
--- pinsets/page.pinsets.php
+++ pinsets/page.pinsets.php
@@ -4,129 +4,125 @@ $tabindex = 0;
 isset($_REQUEST['action'])?$action = $_REQUEST['action']:$action='';
 
 //the item we are currently displaying
-isset($_REQUEST['itemid'])?$itemid=$_REQUEST['itemid']:$itemid='';
+isset($_REQUEST['extdisplay'])?$extdisplay=$_REQUEST['extdisplay']:$extdisplay='';
 
 $dispnum = "pinsets"; //used for switch on config.php
 
 //if submitting form, update database
 if(isset($_POST['action'])) {
-	switch ($action) {
-		case "add":
-			pinsets_add($_POST);
-			needreload();
-			redirect_standard();
-		break;
-		case "delete":
-			pinsets_del($itemid);
-			needreload();
-			redirect_standard();
-		break;
-		case "edit":
-			pinsets_edit($itemid,$_POST);
-			needreload();
-			redirect_standard('itemid');
-		break;
-	}
+    switch ($action) {
+        case "add":
+            pinsets_add($_POST);
+            needreload();
+            $_SESSION['msg']=base64_encode(dgettext('amp','Item has been added'));
+            $_SESSION['msgtype']='success';
+            redirect_standard();
+        break;
+        case "delete":
+            pinsets_del($extdisplay);
+            needreload();
+            $_SESSION['msg']=base64_encode(dgettext('amp','Item has been deleted'));
+            $_SESSION['msgtype']='warning';
+            redirect_standard();
+        break;
+        case "edit":
+            pinsets_edit($extdisplay,$_POST);
+            needreload();
+            $_SESSION['msg']=base64_encode(dgettext('amp','Item has been saved'));
+            $_SESSION['msgtype']='success';
+            redirect_standard('extdisplay');
+        break;
+    }
 }
 
 //get list of time conditions
-$pinsetss = pinsets_list();
+
+$rnaventries = array();
+$pinsetss    = pinsets_list();
+foreach ($pinsetss as $pinsets) {
+    $rnaventries[] = array($pinsets['pinsets_id'],$pinsets['description'],'');
+}
+drawListMenu($rnaventries, $type, $display, $extdisplay);
 ?>
 
 <!-- right side menu -->
-<div class="rnav"><ul>
-    <li><a class="<?php echo ($itemid=='' ? 'current':'') ?>" href="config.php?display=<?php echo urlencode($dispnum)?>"><?php echo _("Add PIN Set")?></a></li>
+<!--div class="rnav"><ul>
+    <li><a class="<?php echo ($extdisplay=='' ? 'current':'') ?>" href="config.php?display=<?php echo urlencode($dispnum)?>"><?php echo _("Add PIN Set")?></a></li>
 <?php
 if (isset($pinsetss)) {
-	foreach ($pinsetss as $pinsets) {
-		echo "<li><a class=\"".($itemid==$pinsets['pinsets_id'] ? 'current':'')."\" href=\"config.php?display=".urlencode($dispnum)."&itemid=".urlencode($pinsets['pinsets_id'])."\">{$pinsets['description']}</a></li>";
-	}
+    foreach ($pinsetss as $pinsets) {
+        echo "<li><a class=\"".($extdisplay==$pinsets['pinsets_id'] ? 'current':'')."\" href=\"config.php?display=".urlencode($dispnum)."&extdisplay=".urlencode($pinsets['pinsets_id'])."\">{$pinsets['description']}</a></li>";
+    }
 }
 ?>
-</ul></div>
+</ul></div-->
+<div class='content'>
 <?php
-if ($action == 'delete') {
-	echo '<br><h3>'._("PIN Set ").' '.$itemid.' '._("deleted").'!</h3>';
-} else {
-	if ($itemid){
-		//get details for this time condition
-		$thisItem = pinsets_get($itemid);
-	}
+$helptext = _("PIN Sets are used to manage lists of PINs that can be used to access restricted features such as Outbound Routes. The PIN can also be added to the CDR record's 'accountcode' field."); 
+$help = '<div class="infohelp">?<span style="display:none;">'.$helptext.'</span></div>';
 
-	$delURL = '?'.$_SERVER['QUERY_STRING'].'&action=delete';
-	$delButton = "
-			<form name=delete action=\"\" method=POST>
-				<input type=\"hidden\" name=\"display\" value=\"{$dispnum}\">
-				<input type=\"hidden\" name=\"itemid\" value=\"{$itemid}\">
-				<input type=\"hidden\" name=\"action\" value=\"delete\">
-				<input type=submit value=\""._("Delete PIN Set")."\">
-			</form>";
+if ($extdisplay){
+    //get details for this time condition
+    $thisItem = pinsets_get($extdisplay);
+    echo "<div class='is-flex'><h2>"._("Edit PIN Set").": ".$thisItem['description']."</h2>$help</div>";
+} else {
+    echo "<div class='is-flex'><h2>"._("Add PIN Set")."</h2>$help</div>";
+
+}
 
 ?>
 
-	<h2><?php echo ($itemid ? _("PIN Set:")." ". $itemid : _("Add PIN Set")); ?></h2>
+<form id="mainform" autocomplete="off" name="edit" action="" method="post" onsubmit="return edit_onsubmit(this);">
+    <input type="hidden" name="display" value="<?php echo $dispnum?>">
+    <input type="hidden" name="action" value="<?php echo ($extdisplay ? 'edit' : 'add') ?>">
+    <input type="hidden" name="deptname" value="<?php echo $_SESSION["AMP_user"]->_deptname ?>">
 
-	<p><?php echo ($itemid ? '' : _("PIN Sets are used to manage lists of PINs that can be used to access restricted features such as Outbound Routes. The PIN can also be added to the CDR record's 'accountcode' field.")); ?></p>
+    <table class='table is-borderless is-narrow'>
+    <tr><td colspan="2"><h5><?php echo dgettext('amp','General Settings')?></h5></td></tr>
 
-<?php		if ($itemid){  echo $delButton; 	} ?>
+<?php if ($extdisplay){ ?>
+        <input type="hidden" name="account" value="<?php echo $extdisplay; ?>">
+<?php } ?>
 
-<form autocomplete="off" name="edit" action="" method="post" onsubmit="return edit_onsubmit();">
-	<input type="hidden" name="display" value="<?php echo $dispnum?>">
-	<input type="hidden" name="action" value="<?php echo ($itemid ? 'edit' : 'add') ?>">
-	<input type="hidden" name="deptname" value="<?php echo $_SESSION["AMP_user"]->_deptname ?>">
+    <tr>
+        <td><?php echo _("PIN Set Description:")?></td>
+        <td><input type="text" class="input w100" name="description" value="<?php echo (isset($thisItem['description']) ? $thisItem['description'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+    </tr>
+    <tr>
+        <td><a href="#" class="info"><?php echo _("Record In CDR?:")?><span><?php echo _("Select this box if you would like to record the PIN in the call detail records when used")?></span></a></td>
+        <td><div class="field"><input type="checkbox" class="switch" id="chkaddtocdr" name="addtocdr" value="1" <?php echo (isset($thisItem['addtocdr']) && $thisItem['addtocdr'] == '1' ? 'CHECKED' : ''); ?> tabindex="<?php echo ++$tabindex;?>"><label for="chkaddtocdr"> </label></div></td>
+    </tr>
+    <tr>
+        <td><a href="#" class="info"><?php echo _("PIN List:")?><span><?php echo _("Enter a list of one or more PINs.  One PIN per line.")?></span></a></td>
+        <td>
+            <textarea class="textarea" name="passwords" tabindex="<?php echo ++$tabindex;?>"><?php echo (isset($thisItem['passwords']) ? $thisItem['passwords'] : ''); ?></textarea>
+        </td>
+    </tr>
 
-	<table>
-	<tr><td colspan="2"><h5><?php echo ($itemid ? _("Edit PIN Set") : _("New PIN Set")) ?></h5></td></tr>
+    </table>
+</form>
+<script>
 
-<?php		if ($itemid){ ?>
-		<input type="hidden" name="account" value="<?php echo $itemid; ?>">
-<?php		}?>
+function edit_onsubmit(theForm) {
 
-	<tr>
-		<td><?php echo _("PIN Set Description:")?></td>
-		<td><input type="text" size=23 name="description" value="<?php echo (isset($thisItem['description']) ? $thisItem['description'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
-	</tr>
-	<tr>
-		<td><a href="#" class="info"><?php echo _("Record In CDR?:")?><span><?php echo _("Select this box if you would like to record the PIN in the call detail records when used")?></span></a></td>
-		<td><input type="checkbox" name="addtocdr" value="1" <?php echo (isset($thisItem['addtocdr']) && $thisItem['addtocdr'] == '1' ? 'CHECKED' : ''); ?> tabindex="<?php echo ++$tabindex;?>"></td>
-	</tr>
-	<tr>
-		<td><a href="#" class="info"><?php echo _("PIN List:")?><span><?php echo _("Enter a list of one or more PINs.  One PIN per line.")?></span></a></td>
-		<td>
-			<textarea rows=15 cols=20 name="passwords" tabindex="<?php echo ++$tabindex;?>"><?php echo (isset($thisItem['passwords']) ? $thisItem['passwords'] : ''); ?></textarea>
-		</td>
-	</tr>
+    defaultEmptyOK = false;
 
-	<tr>
-		<td colspan="2"><br><h6><input name="submit" type="submit" value="<?php echo _("Submit Changes")?>" tabindex="<?php echo ++$tabindex;?>"></h6></td>
-	</tr>
-	</table>
-<script language="javascript">
-<!--
+    <?php if (function_exists('module_get_field_size')) { ?>
+        var sizeDisplayName = "<?php echo module_get_field_size('pinsets', 'description', 50); ?>";
+        if (!isCorrectLength(theForm.description.value, sizeDisplayName))
+            return warnInvalid(theForm.description, "<?php echo _('The PIN Set Description provided is too long.'); ?>")
+    <?php } ?>
 
-var theForm = document.edit;
-theForm.description.focus();
+    if (!isAlphanumeric(theForm.description.value))
+        return warnInvalid(theForm.description, "<?php _("Please enter a valid Description") ?>");
 
-function edit_onsubmit() {
-
-	defaultEmptyOK = false;
-
-	<?php if (function_exists('module_get_field_size')) { ?>
-		var sizeDisplayName = "<?php echo module_get_field_size('pinsets', 'description', 50); ?>";
-		if (!isCorrectLength(theForm.description.value, sizeDisplayName))
-			return warnInvalid(theForm.description, "<?php echo _('The PIN Set Description provided is too long.'); ?>")
-	<?php } ?>
-
-	if (!isAlphanumeric(theForm.description.value))
-		return warnInvalid(theForm.description, "<?php _("Please enter a valid Description") ?>");
-
-	return true;
+    $.LoadingOverlay('show');
+    return true;
 }
 
--->
+<?php echo js_display_confirmation_toasts(); ?>
+</script>
+</div>
+<?php echo form_action_bar($extdisplay); ?>
 </script>
 
-	</form>
-<?php
-} //end if action == delete
-?>
