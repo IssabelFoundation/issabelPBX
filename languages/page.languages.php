09a15cd4ddb8f075cc29f9bdcc2213c1fab8849e
Nicolás Gudiño <nicolas@house.com.ar>
2022-08-28 12:22:18 -0300

    Remove colon from labels

--- languages/page.languages.php
+++ languages/page.languages.php
@@ -77,11 +77,11 @@ if ($extdisplay) {
     <table class='table is-borderless is-narrow'>
     <tr><td colspan="2"><h5><?php echo dgettext('amp','General Settings');?></h5></td></tr>
 	<tr>
-		<td><a href="#" class="info"><?php echo _("Description")?>:<span><?php echo _("The descriptive name of this language instance. For example \"French Main IVR\"")?></span></a></td>
+		<td><a href="#" class="info"><?php echo _("Description")?><span><?php echo _("The descriptive name of this language instance. For example \"French Main IVR\"")?></span></a></td>
 		<td><input autofocus class='input w100' type="text" name="description" value="<?php  echo $description; ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
 	<tr>
-		<td><a href="#" class="info"><?php echo _("Language Code")?>:<span><?php echo _("The Asterisk language code you want to change to. For example \"fr\" for French, \"de\" for German")?></span></a></td>
+		<td><a href="#" class="info"><?php echo _("Language Code")?><span><?php echo _("The Asterisk language code you want to change to. For example \"fr\" for French, \"de\" for German")?></span></a></td>
 		<td><input class="input w100" type="text" name="lang_code" value="<?php echo $lang_code; ?>"  tabindex="<?php echo ++$tabindex;?>"/></td> </tr>
 	<tr><td colspan="2"><br><h5><?php echo _("Destination")?></h5></td></tr>
 
