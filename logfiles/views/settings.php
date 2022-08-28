0c935c6c8594ca22256df68b86db30e7059270bf
Nicolás Gudiño <nicolas@house.com.ar>
2022-08-28 11:51:06 -0300

    Spacing/Formatting

--- logfiles/views/settings.php
+++ logfiles/views/settings.php
@@ -6,30 +6,29 @@ $html = '<div class="content">';
 $html .= heading(_('Log File Settings'), 2);
 $html .= form_open($_SERVER['REQUEST_URI'], '', array('action' => 'save'));
 //general settings
-//$html .= heading(_('General Settings'), 5);
 $table = new CI_Table;
 $table->set_template(array('table_open' => '<table class="table is-narrow is-borderless" id="logfile_settings">'));
 
 $table->add_row(array('colspan' => 2, 'data' => heading(dgettext('amp','General Settings'), 5) ));
 //date format
 $data = array(
-			'name'			=> 'dateformat',
-			//'id'			=> 'username',
-			'value'			=> $dateformat,
-			'placeholder'	=> 'Date Format'
+            'name'            => 'dateformat',
+            //'id'            => 'username',
+            'value'            => $dateformat,
+            'placeholder'    => 'Date Format'
             );
 $label = ipbx_label(_('Date Format'), _('Customize the display of debug message time stamps. '
-									. 'See strftime(3) Linux manual for format specifiers. '
-									. 'Note that there is also a fractional second parameter '
-									. 'which may be used in this field.  Use %1q for tenths, '
-									. '%2q for hundredths, etc.')
-									. br() . _('Leave blank for default: ISO 8601 date format '
-									. 'yyyy-mm-dd HH:MM:SS (%F %T)'));
+                                    . 'See strftime(3) Linux manual for format specifiers. '
+                                    . 'Note that there is also a fractional second parameter '
+                                    . 'which may be used in this field.  Use %1q for tenths, '
+                                    . '%2q for hundredths, etc.')
+                                    . br() . _('Leave blank for default: ISO 8601 date format '
+                                    . 'yyyy-mm-dd HH:MM:SS (%F %T)'));
 $table->add_row($label, form_input($data,'',' class="input"'));
 
 $help_li[] = _('Sequential: Rename archived logs in order, such that the newest has the highest sequence number.');
 $help_li[] = _('Rotate: Rotate all the old files, such that the oldest has the highest sequence '
-			. 'number (expected behavior for Unix administrators).');
+            . 'number (expected behavior for Unix administrators).');
 $help_li[] = _('Timestamp: Rename the logfiles using a timestamp instead of a sequence number when "logger rotate" is executed.');
 $label = ipbx_label(_('Log rotation'), _('Log rotation strategy: ') . ul($help_li));
 $table->add_row($label, ipbx_radio('rotatestrategy',array(array('value'=>'sequential','text'=>_('Sequential')),array('value'=>'rotate','text'=>_('Rotate')),array('value'=>'timestamp','text'=>_('Timestamp'))),$rotatestrategy,false));
@@ -51,81 +50,71 @@ $table->set_template(array('table_open' => '<table class="table is-narrow notfix
 //draw table header with help on every option
 $has_security_option = version_compare($amp_conf['ASTVERSION'],'11.0','ge');
 $heading = array(
-			ipbx_label(_('File Name'), _('Name of file. Use absolute path for a different location.')),
-			ipbx_label(_('Debug'), 'debug: ' . _('Messages used for debuging. '
-									. 'Do not report these as error\'s unless you have a '
-									. 'specific issue that you are attempting to debug. '
-									. 'Also note that Debug messages are also very verbose '
-									. 'and can and do fill up logfiles (and disk storage) quickly.')),
-			ipbx_label(_('DTMF'), 'dtmf: ' . _('Keypresses as understood by asterisk. Usefull for debuging IVR and VM issues.')),
-			ipbx_label(_('Error'), 'error: ' . _('Critical errors and issues')),
-			ipbx_label(_('Fax'), 'fax: ' . _('Transmition and receiving of faxes')),
-			ipbx_label(_('Notice'), 'notice: ' . _('Messages of specific actions, such as a phone registration or call completion')),
-			ipbx_label(_('Verbose'), 'verbose: ' . _('Step-by-step messages of every step of a call flow. '
-										. 'Always enable and review if calls dont flow as expected')),
-			ipbx_label(_('Warning'), 'warning: ' . _('Possible issues with dialplan syntaxt or call flow, but not critical.'))
-		);
+            ipbx_label(_('File Name'), _('Name of file. Use absolute path for a different location.')),
+            ipbx_label(_('Debug'), 'debug: ' . _('Messages used for debuging. '
+                                    . 'Do not report these as error\'s unless you have a '
+                                    . 'specific issue that you are attempting to debug. '
+                                    . 'Also note that Debug messages are also very verbose '
+                                    . 'and can and do fill up logfiles (and disk storage) quickly.')),
+            ipbx_label(_('DTMF'), 'dtmf: ' . _('Keypresses as understood by asterisk. Usefull for debuging IVR and VM issues.')),
+            ipbx_label(_('Error'), 'error: ' . _('Critical errors and issues')),
+            ipbx_label(_('Fax'), 'fax: ' . _('Transmition and receiving of faxes')),
+            ipbx_label(_('Notice'), 'notice: ' . _('Messages of specific actions, such as a phone registration or call completion')),
+            ipbx_label(_('Verbose'), 'verbose: ' . _('Step-by-step messages of every step of a call flow. '
+                                        . 'Always enable and review if calls dont flow as expected')),
+            ipbx_label(_('Warning'), 'warning: ' . _('Possible issues with dialplan syntaxt or call flow, but not critical.'))
+        );
 
 if ($has_security_option) { 
-	$heading[] = ipbx_label(_('Security'), 'security: ' . _('Notification of security related events such as authentication attempts.')); 
+    $heading[] = ipbx_label(_('Security'), 'security: ' . _('Notification of security related events such as authentication attempts.')); 
 }
 
 $heading[] = ipbx_label(_('Delete'));
-//$table->set_heading($heading);
 $table->add_row($heading);
 
 
 //actual log files
 $count = 0;
-//$logfiles[] = array('name' => 'test', 'debug' => 'off', 'dtmf' => 'off', 'error' => 'on', 'fax' => 'off', 'notice' => 'on', 'verbose' => 'on', 'warning' => 'on');
 
 foreach ($logfiles as $l) {
-	$row[] = form_input(
-				array(
-					'name'			=> 'logfiles[name][]',
-					'value'			=> $l['name'],
-                    'placeholder'	=> _('file path/name'),
+    $row[] = form_input(
+                array(
+                    'name'            => 'logfiles[name][]',
+                    'value'            => $l['name'],
+                    'placeholder'    => _('file path/name'),
                     'class'         => 'input',
-					'required'		=> ''
-				)
-			);
-	
-	$onoff = array(
-			'on'	=> _('On'),
-			'off'	=> _('Off')
-	);
+                    'required'        => ''
+                )
+            );
+    
+    $onoff = array(
+            'on'    => _('On'),
+            'off'    => _('Off')
+    );
 
-	$row[] = form_dropdown('logfiles[debug][]', $onoff, $l['debug'],' class="componentSelectAutoWidthNoSearch" ');
-	$row[] = form_dropdown('logfiles[dtmf][]', $onoff, $l['dtmf'],' class="componentSelectAutoWidthNoSearch" ');
-	$row[] = form_dropdown('logfiles[error][]', $onoff, $l['error'],' class="componentSelectAutoWidthNoSearch" ');
-	$row[] = form_dropdown('logfiles[fax][]', $onoff, $l['fax'],' class="componentSelectAutoWidthNoSearch" ');
-	$row[] = form_dropdown('logfiles[notice][]', $onoff, $l['notice'],' class="componentSelectAutoWidthNoSearch" ');
-	$row[] = form_dropdown('logfiles[verbose][]', $onoff, $l['verbose'],' class="componentSelectAutoWidthNoSearch" ');
+    $row[] = form_dropdown('logfiles[debug][]', $onoff, $l['debug'],' class="componentSelectAutoWidthNoSearch" ');
+    $row[] = form_dropdown('logfiles[dtmf][]', $onoff, $l['dtmf'],' class="componentSelectAutoWidthNoSearch" ');
+    $row[] = form_dropdown('logfiles[error][]', $onoff, $l['error'],' class="componentSelectAutoWidthNoSearch" ');
+    $row[] = form_dropdown('logfiles[fax][]', $onoff, $l['fax'],' class="componentSelectAutoWidthNoSearch" ');
+    $row[] = form_dropdown('logfiles[notice][]', $onoff, $l['notice'],' class="componentSelectAutoWidthNoSearch" ');
+    $row[] = form_dropdown('logfiles[verbose][]', $onoff, $l['verbose'],' class="componentSelectAutoWidthNoSearch" ');
     $row[] = form_dropdown('logfiles[warning][]', $onoff, $l['warning'],' class="componentSelectAutoWidthNoSearch" ');
 
-	if ($has_security_option) { 
-		$row[] = form_dropdown('logfiles[security][]', $onoff, $l['security'],' class="componentSelectAutoWidthNoSearch" '); 
+    if ($has_security_option) { 
+        $row[] = form_dropdown('logfiles[security][]', $onoff, $l['security'],' class="componentSelectAutoWidthNoSearch" '); 
     }
     $row[] = "<button name='del$count' id='del$count' value='Delete' class='button is-small is-danger delete_entry' data-tooltip='"._('Delete')."'><span class='icon is-small'><i class='fa fa-trash'></i></span></button>";
 
-        /*
-	$row[] = '<img src="images/trash.png" style="cursor:pointer" title="' 
-			. _('Delete this entry. Click Submit to save changes') 
-            . '" class="delete_entry">';
-         */
-	$table->add_row(array_values($row));
+    $table->add_row(array_values($row));
     unset($row);
     $count++;
 }
 
 $html .= $table->generate() ;
-//$html .= '<img class="IVREntries" src="/admin/modules/logfiles/assets/images/add.png" style="cursor:pointer" title="' . _('New Log File') . '" id="add_entry">';
 
 $html .= '<button type="button" class="button is-small is-rounded" id="add_entry">'._('New Log File').'</button>';
 
-//$html .= br(4) . form_submit('save', _('Save'));
 $html .= form_action_bar('');
-
 
 $html .= form_close();
 $html .= '<script src="/admin/modules/logfiles/assets/js/views/settings.js"></script>';
