b8b38054cfba63a5860cd8f4db27a61405d5a2cd
Nicolás Gudiño <nicolas@house.com.ar>
2022-09-02 19:27:56 -0300

    Use switchboxes instead of checkboxes, remove dead code

--- manager/page.manager.php
+++ manager/page.manager.php
@@ -15,42 +15,46 @@ $astver =  $engineinfo['version'];
 $ast_ge_16 = version_compare($astver, '1.6', 'ge');
 $ast_ge_11 = version_compare($astver, '11', 'ge');
 
+$tabindex = 0;
+
 //if submitting form, update database
 global $amp_conf;
+
 if($action == 'add' || $action == 'delete') {
-	$ampuser = $amp_conf['AMPMGRUSER'];
-	if($ampuser == $name) {
-		$action = 'conflict';
-	}
+    $ampuser = $amp_conf['AMPMGRUSER'];
+    if($ampuser == $name) {
+        $action = 'conflict';
+    }
 }
+
 switch ($action) {
-	case "add":
-		$rights = manager_format_in($_REQUEST);
+    case "add":
+        $rights = manager_format_in($_REQUEST);
         manager_add($name,$secret,$deny,$permit,$rights['read'],$rights['write']);
         needreload();
         $_SESSION['msg']=base64_encode(dgettext('amp','Item has been added'));
         $_SESSION['msgtype']='success';
         redirect_standard();
-	break;
-	case "delete":
-		manager_del($extdisplay);
-		needreload();
+    break;
+    case "delete":
+        manager_del($extdisplay);
+        needreload();
         $_SESSION['msg']=base64_encode(dgettext('amp','Item has been deleted'));
         $_SESSION['msgtype']='warning';
         redirect_standard();
-	break;
-	case "edit":  //just delete and re-add
-		manager_del($name);
-		$rights = manager_format_in($_REQUEST);
-		manager_add($name,$secret,$deny,$permit,$rights['read'],$rights['write']);
-		needreload();
+    break;
+    case "edit":  //just delete and re-add
+        manager_del($name);
+        $rights = manager_format_in($_REQUEST);
+        manager_add($name,$secret,$deny,$permit,$rights['read'],$rights['write']);
+        needreload();
         $_SESSION['msg']=base64_encode(dgettext('amp','Item has been saved'));
         $_SESSION['msgtype']='success';
         redirect_standard('extdisplay');
-	break;
-	case "conflict":
-		//do nothing we are conflicting with the IssabelPBX Asterisk Manager User
-	break;
+    break;
+    case "conflict":
+        //do nothing we are conflicting with the IssabelPBX Asterisk Manager User
+    break;
 }
 
 $managers = manager_list();
@@ -61,202 +65,125 @@ foreach($managers as $manager) {
 }
 drawListMenu($rnaventries, $type, $display, $extdisplay);
 ?>
-<!--div class="rnav"><ul>
-    <li><a class="<?php echo ($extdisplay=='' ? 'current':'') ?>" href="config.php?type=tool&amp;display=<?php echo urlencode($dispnum)?>"><?php echo _("Add Manager")?></a></li>
-<?php
-if (isset($managers)) {
-	foreach ($managers as $manager) {
-		echo "<li><a class=\"".($extdisplay==$manager['name'] ? 'current':'')."\" href=\"config.php?type=tool&amp;display=".urlencode($dispnum)."&extdisplay=".$manager['name']."\">{$manager['name']}</a></li>";
-	}
-}
-?>
-<ul></div-->
 <div class='content'>
 <?php
 
-/*
-if ($action == 'delete') {
-	echo '<br><h3>'._("Manager").' '.$extdisplay.' '._("deleted").'!</h3><br><br><br><br><br><br><br><br>';
-} elseif($action == 'conflict') {
-	echo '<br><h3>'.sprintf(_("Conflicting IssabelPBX Manager of %s has not been added"),$name).'!</h3><br><br><br><br><br><br><br><br>';
+if ($extdisplay){
+    //get details for this manager
+    $thisManager = manager_get($extdisplay);
+    extract(manager_format_out($thisManager));
+    echo "<h2>"._("Edit Manager").": ".$extdisplay."</h2>\n";
 } else {
- */
+    echo "<h2>"._("Add Manager")."</h2>\n";
+}
 
-	if ($extdisplay){
-		//get details for this manager
-		$thisManager = manager_get($extdisplay);
-		//create variables
-		extract(manager_format_out($thisManager));
-	}
-
-	$delURL = $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'].'&action=delete';
 ?>
-
-
-<?php		if ($extdisplay){ ?>
-	<h2><?php echo _("Edit Manager").": ". $extdisplay; ?></h2>
-<?php		} else { ?>
-	<h2><?php echo _("Add Manager"); ?></h2>
-<?php		}
-			$tabindex = 0;
-?>
-	<form id="mainform" autocomplete="off" name="editMan" action="<?php $_SERVER['PHP_SELF'] ?>" method="post" onsubmit="return checkConf(this);">
-	<input type="hidden" name="display" value="<?php echo $dispnum?>">
-	<input type="hidden" name="action" value="<?php echo ($extdisplay ? 'edit' : 'add') ?>">
+    <form id="mainform" autocomplete="off" name="editMan" action="<?php $_SERVER['PHP_SELF'] ?>" method="post" onsubmit="return checkConf(this);">
+    <input type="hidden" name="display" value="<?php echo $dispnum?>">
+    <input type="hidden" name="action" value="<?php echo ($extdisplay ? 'edit' : 'add') ?>">
     <table class='table is-narrow is-borderless'>
     <tr><td colspan="2"><h5><?php echo dgettext('amp','General Settings');?></h5></td></tr>
-	<tr>
-		<td><a href="#" class="info"><?php echo _("Manager name")?><span><?php echo _("Name of the manager without space.")?></span></a></td>
-		<td><input class="input w100" autofocus type="text" name="name" value="<?php echo (isset($name) ? $name : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
-	</tr>
-	<tr>
-		<td><a href="#" class="info"><?php echo _("Manager secret")?><span><?php echo _("Password for the manager.")?></span></a></td>
-		<td><input class="input w100" type="text" name="secret" value="<?php echo (isset($secret) ? $secret : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
-	</tr>
-	<tr>
-		<td><a href="#" class="info"><?php echo _("Deny")?><span><?php echo _("If you want to deny many hosts or networks, use & char as separator.<br/><br/>Example: 192.168.1.0/255.255.255.0&10.0.0.0/255.0.0.0")?></span></a></td>
-		<td><input class="input w100" type="text" name="deny" value="<?php echo (isset($deny) ? $deny : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
-	</tr>
-	<tr>
-		<td><a href="#" class="info"><?php echo _("Permit")?><span><?php echo _("If you want to permit many hosts or networks, use & char as separator. Look at deny example.")?></span></a></td>
-		<td><input class="input w100" type="text" name="permit" value="<?php echo (isset($permit) ? $permit : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
-	</tr>
-	<tr>
-		<td colspan="2"><h5><?php echo _("Rights")?></h5></td>
-	</tr>
-	<tr>
-		<td colspan="2">
-		<table>
-			<tr><th></th><th><?php echo _("Read")?></th><th><?php echo _("Write")?></th></tr>
-			<tr>
-				<td><a href="#" class="info">system<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rsystem" <?php echo (isset($rsystem)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wsystem" <?php echo (isset($wsystem)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">call<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rcall" <?php echo (isset($rcall)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wcall" <?php echo (isset($wcall)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">log<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rlog" <?php echo (isset($rlog)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wlog" <?php echo (isset($wlog)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">verbose<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rverbose" <?php echo (isset($rverbose)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wverbose" <?php echo (isset($wverbose)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">command<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rcommand" <?php echo (isset($rcommand)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wcommand" <?php echo (isset($wcommand)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">agent<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="ragent" <?php echo (isset($ragent)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wagent" <?php echo (isset($wagent)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">user<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="ruser" <?php echo (isset($ruser)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wuser" <?php echo (isset($wuser)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-<?php
-  if ($ast_ge_16) {
+    <tr>
+        <td><a href="#" class="info"><?php echo _("Manager name")?><span><?php echo _("Name of the manager without space.")?></span></a></td>
+        <td><input class="input w100" autofocus type="text" name="name" value="<?php echo (isset($name) ? $name : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+    </tr>
+    <tr>
+        <td><a href="#" class="info"><?php echo _("Manager secret")?><span><?php echo _("Password for the manager.")?></span></a></td>
+        <td><input class="input w100" type="text" name="secret" value="<?php echo (isset($secret) ? $secret : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+    </tr>
+    <tr>
+        <td><a href="#" class="info"><?php echo _("Deny")?><span><?php echo _("If you want to deny many hosts or networks, use & char as separator.<br/><br/>Example: 192.168.1.0/255.255.255.0&10.0.0.0/255.0.0.0")?></span></a></td>
+        <td><input class="input w100" type="text" name="deny" value="<?php echo (isset($deny) ? $deny : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+    </tr>
+    <tr>
+        <td><a href="#" class="info"><?php echo _("Permit")?><span><?php echo _("If you want to permit many hosts or networks, use & char as separator. Look at deny example.")?></span></a></td>
+        <td><input class="input w100" type="text" name="permit" value="<?php echo (isset($permit) ? $permit : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+    </tr>
+    <tr>
+        <td colspan="2"><h5><?php echo _("Rights")?></h5></td>
+    </tr>
+    <tr>
+        <td colspan="2">
+        <table class='table is-narrow notfixed'>
+            <tr><th></th><th><?php echo _("Read")?></th><th><?php echo _("Write")?></th></tr>
+
+<?php 
+    $mgrperms = array('system','call','log','verbose','command','agent','user');
+    if ($ast_ge_16) {
+        $mgrperms = array_merge($mgrperms,array('config','dtmf','reporting','cdr','dialplan','originate'));
+    }
+    if ($ast_ge_11) {
+        $mgrperms = array_merge($mgrperms,array('message'));
+    }
+
+    foreach($mgrperms as $perm) {
+         $rname = "r$perm";
+         $wname = "w$perm";
+
+         echo "<tr>\n";
+         echo "  <td><a href='javascript:void(0)' class='info'>$perm<span>"._("Check Asterisk documentation.")."</span></a></td>\n";
+         echo "  <td>";
+         echo form_switch($rname, 'true', isset($$rname));
+         echo "  </td>\n";
+         echo "  <td>";
+         echo form_switch($wname, 'true', isset($$wname));
+         echo "  </td>\n";
+         echo "</tr>\n";
+    }
 ?>
-			<tr>
-				<td><a href="#" class="info">config<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rconfig" <?php echo (isset($rconfig)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wconfig" <?php echo (isset($wconfig)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">dtmf<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rdtmf" <?php echo (isset($rdtmf)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wdtmf" <?php echo (isset($wdtmf)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">reporting<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rreporting" <?php echo (isset($rreporting)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wreporting" <?php echo (isset($wreporting)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">cdr<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rcdr" <?php echo (isset($rcdr)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wcdr" <?php echo (isset($wcdr)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">dialplan<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rdialplan" <?php echo (isset($rdialplan)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wdialplan" <?php echo (isset($wdialplan)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-			<tr>
-				<td><a href="#" class="info">originate<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="roriginate" <?php echo (isset($roriginate)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="woriginate" <?php echo (isset($woriginate)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
+            <tr>
+                <td><a href="#" class="info">ALL<span><?php echo _("Check All/None.")?></span></a></td>
+                <td><?php echo form_switch('allnoner',false); ?></td>
+                <td><?php echo form_switch('allnonew',false); ?></td>
+            </tr>
+        </table>
+        </td>
+    </tr>
 
-<?php // if 1.6 add these
-  }
-?>
-<?php
-	if ($ast_ge_11) {
-?>
-			<tr>
-				<td><a href="#" class="info">message<span><?php echo _("Check Asterisk documentation.")?></span></a></td>
-				<td><input type="checkbox" class="rpermission" name="rmessage" <?php echo (isset($rmessage)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-				<td><input type="checkbox" class="wpermission" name="wmessage" <?php echo (isset($wmessage)?"checked":'');?> tabindex="<?php echo ++$tabindex;?>"></td>
-			</tr>
-<?php // if 1.6 add these
-	}
-?>
-			<tr>
-				<td><a href="#" class="info">ALL<span><?php echo _("Check All/None.")?></span></a></td>
-				<td><input type="checkbox" id="rallnone" name="rallnone"></td>
-				<td><input type="checkbox" id="wallnone" name="wallnone"></td>
-			</tr>
-		</table>
-		</td>
-	</tr>
+    </table>
+    </form>
+    <script>
+    var theForm = document.editMan;
+    
+    $('input[name=allnonew]').on('change',function() {
+        that = this;
+        $('input[type=checkbox]').each(function() { 
+            type = $(this).attr('name').substr(0,1); 
+            if(type=='w') { $(this).prop('checked',$(that).is(':checked')); }
+        });
+    });
 
-	</table>
-	</form>
-	<script>
-	var theForm = document.editMan;
+    $('input[name=allnoner]').on('change',function() {
+        that = this;
+        $('input[type=checkbox]').each(function() { 
+            type = $(this).attr('name').substr(0,1); 
+            if(type=='r') { $(this).prop('checked',$(that).is(':checked')); }
+        });
+    });
 
-	$('#rallnone').on('change',function() {
-		$('.rpermission').prop('checked',$(this).is(':checked'));
-	});
+    function checkConf(theForm)
+    {
+        var errName = "<?php echo _('The manager name cannot be empty or may not have any space in it.'); ?>";
+        var errSecret = "<?php echo _('The manager secret cannot be empty.'); ?>";
+        var errDeny = "<?php echo _('The manager deny is not well formatted.'); ?>";
+        var errPermit = "<?php echo _('The manager permit is not well formatted.'); ?>";
+        var errRead = "<?php echo _('The manager read field is not well formatted.'); ?>";
+        var errWrite = "<?php echo _('The manager write field is not well formatted.'); ?>";
 
-	$('#wallnone').on('change',function() {
-		$('.wpermission').prop('checked',$(this).is(':checked'));
-	});
-
-	function checkConf(theForm)
-	{
-		var errName = "<?php echo _('The manager name cannot be empty or may not have any space in it.'); ?>";
-		var errSecret = "<?php echo _('The manager secret cannot be empty.'); ?>";
-		var errDeny = "<?php echo _('The manager deny is not well formatted.'); ?>";
-		var errPermit = "<?php echo _('The manager permit is not well formatted.'); ?>";
-		var errRead = "<?php echo _('The manager read field is not well formatted.'); ?>";
-		var errWrite = "<?php echo _('The manager write field is not well formatted.'); ?>";
-
-		defaultEmptyOK = false;
-		if ((theForm.name.value.search(/\s/) >= 0) || (theForm.name.value.length == 0))
-			return warnInvalid(theForm.name, errName);
-		if (theForm.secret.value.length == 0)
-			return warnInvalid(theForm.name, errSecret);
-		// Only IP/MASK format are checked
-		if (theForm.deny.value.search(/\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b(&\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b)*$/))
-			return warnInvalid(theForm.name, errDeny);
-		if (theForm.permit.value.search(/\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b(&\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b)*$/))
+        defaultEmptyOK = false;
+        if ((theForm.name.value.search(/\s/) >= 0) || (theForm.name.value.length == 0))
+            return warnInvalid(theForm.name, errName);
+        if (theForm.secret.value.length == 0)
+            return warnInvalid(theForm.name, errSecret);
+        // Only IP/MASK format are checked
+        if (theForm.deny.value.search(/\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b(&\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b)*$/))
+            return warnInvalid(theForm.name, errDeny);
+        if (theForm.permit.value.search(/\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b(&\b(?:\d{1,3}\.){3}\d{1,3}\b\/\b(?:\d{1,3}\.){3}\d{1,3}\b)*$/))
             return warnInvalid(theForm.name, errPermit);
 
         $.LoadingOverlay('show');
-		return true;
-	}
-
+        return true;
+    }
 
 <?php echo js_display_confirmation_toasts(); ?>
 </script>
