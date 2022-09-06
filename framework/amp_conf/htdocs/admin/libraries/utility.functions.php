a52d7315ebe55ce342274c74f7aa79dab447d3fc
Nicolás Gudiño <nicolas@house.com.ar>
2022-09-06 20:39:18 -0300

    Report private ips on remote ip

--- framework/amp_conf/htdocs/admin/libraries/utility.functions.php
+++ framework/amp_conf/htdocs/admin/libraries/utility.functions.php
@@ -1594,7 +1594,7 @@ function whatsMyIP() {
     foreach ( array( 'HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
         if (array_key_exists($key, $_SERVER) === true) {
             foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip) {
-                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_RES_RANGE) !== false) {
                     return $ip;
                 }
             }
