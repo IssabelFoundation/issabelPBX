38765383b192c41cba66a1e7d46a13483e154bc0
Nicolás Gudiño <nicolas@house.com.ar>
2022-09-08 11:38:56 -0300

    Fix typo/closing bracket on js code

--- tts/assets/js/zdone.js
+++ tts/assets/js/zdone.js
@@ -1,6 +1,6 @@
 var myCodeMirror;
 
-up.compiler('.content', function(element,data) {})
+up.compiler('.content', function(element,data) {
     if($('#ttstext').length>0) {
         myCodeMirror = CodeMirror.fromTextArea($('#ttstext')[0],{
           lineNumbers: true,
