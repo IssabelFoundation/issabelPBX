6c585bfc610db13f3612caa9bb8f2dc0007c9deb
Nicolás Gudiño <nicolas@house.com.ar>
2023-08-31 13:21:27 -0300

    Add default value to seq field to avoid problems with mariadb STRICT_TRANS_TABLES

--- sipsettings/install.php
+++ sipsettings/install.php
@@ -9,7 +9,7 @@ $sql = <<< END
 CREATE TABLE IF NOT EXISTS `sipsettings` (
   `keyword` VARCHAR (50) NOT NULL default '',
   `data`    VARCHAR (255) NOT NULL default '',
-  `seq`     TINYINT (1),
+  `seq`     TINYINT (1) NOT NULL default '0',
   `type`    TINYINT (1) NOT NULL default '0',
   PRIMARY KEY (`keyword`,`seq`,`type`)
 )
@@ -61,7 +61,7 @@ $sql = <<< END
 CREATE TABLE IF NOT EXISTS `pjsipsettings` (
   `keyword` VARCHAR (50) NOT NULL default '',
   `data`    VARCHAR (255) NOT NULL default '',
-  `seq`     TINYINT (1),
+  `seq`     TINYINT (1) NOT NULL default '0',
   `type`    TINYINT (1) NOT NULL default '0',
   PRIMARY KEY (`keyword`,`seq`,`type`)
 )
