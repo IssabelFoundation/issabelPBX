fe63482e15de23bfb95367ddfd6cf8136f754c6e
Nicolás Gudiño <nicolas@house.com.ar>
2023-09-03 13:04:34 -0300

    Try to set collation on table creation , only for mysql

--- callrecording/install.php
+++ callrecording/install.php
@@ -6,24 +6,27 @@ global $db;
 
 $autoincrement=(preg_match("/qlite/",$amp_conf["AMPDBENGINE"])) ? "AUTOINCREMENT":"AUTO_INCREMENT";
 
-$sql[]="CREATE TABLE IF NOT EXISTS callrecording (
+$sql = array();
+
+$sql['callrecording']="CREATE TABLE IF NOT EXISTS callrecording (
     callrecording_id INTEGER NOT NULL PRIMARY KEY $autoincrement,
     callrecording_mode VARCHAR( 50 ) ,
     description VARCHAR( 50 ) ,
     dest VARCHAR( 255 )
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
+)";
 
-$sql[]="CREATE TABLE IF NOT EXISTS callrecording_module (
+$sql['callrecording_module']="CREATE TABLE IF NOT EXISTS callrecording_module (
     extension varchar(50),
     cidnum varchar(50) default '',
     callrecording varchar(10),
     display varchar(20)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
+)";
 
-foreach($sql as $s){
+foreach($sql as $t=>$s){
+    if(preg_match("/mysql/",$amp_conf["AMPDBENGINE"]))  { $s.=" DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";   }
     $check = $db->query($s);
     if(DB::IsError($check)) {
-        die_issabelpbx("Can not create callrecording table\n");
+        die_issabelpbx("Can not create $t table\n");
     }
 }
 
