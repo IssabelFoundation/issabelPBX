d0b8116530f8559f636afe89566bdb0440b57308
Nicolás Gudiño <nicolas@house.com.ar>
2023-09-04 16:18:24 -0300

    Formatting/Indent. Prevent undef type variable

--- ivr/views/rnav.php
+++ ivr/views/rnav.php
@@ -2,29 +2,23 @@
 
 $li[] = '<a class="'.($id=='' ? 'current':'').'" href="config.php?display='. urlencode($display) . '&action=add">' . __("Add IVR") . '</a>';
 
-
 if (isset($ivr_results)){
-	foreach ($ivr_results as $r) {
-		$r['name'] = $r['name'] ? $r['name'] : 'IVR ID: ' . $r['id'];
-		$li[] = '<a class="' . ( $id == $r['id'] ? 'current' : '') 
-			. '" href="config.php?display=ivr&amp;action=edit&amp;id=' 
-			. $r['id'] . '">' 
-			. $r['name'] .'</a>';
-	}
-}	
+    foreach ($ivr_results as $r) {
+        $r['name'] = $r['name'] ? $r['name'] : 'IVR ID: ' . $r['id'];
+        $li[] = '<a class="' . ( $id == $r['id'] ? 'current' : '')
+            . '" href="config.php?display=ivr&amp;action=edit&amp;id='
+            . $r['id'] . '">'
+            . $r['name'] .'</a>';
+    }
+}
 
-//echo '<div class="rnav">' . ul($li) . '</div>';
-
-
-
-
+$type = '';
 $rnaventries = array();
 foreach ($ivr_results as $r) {
-	$r['name'] = $r['name'] ? $r['name'] : 'IVR ID: ' . $r['id'];
+    $r['name'] = $r['name'] ? $r['name'] : 'IVR ID: ' . $r['id'];
     $rnaventries[] = array($r['id'],$r['name'],'');
 }
 drawListMenu($rnaventries, $type, $display, $extdisplay);
-
 
 ?>
 <div class='content'>
