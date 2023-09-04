8edc7985d365fafc7a7db9b12d9f9cd45b9da0ce
Nicolás Gudiño <nicolas@house.com.ar>
2023-09-03 22:54:46 -0300

    Comment set collation connection for now

--- framework/amp_conf/htdocs/admin/libraries/db_connect.php
+++ framework/amp_conf/htdocs/admin/libraries/db_connect.php
@@ -29,7 +29,7 @@ switch ($amp_conf['AMPDBENGINE']) {
                     . $amp_conf['AMPDBNAME'];
         $db = issabelpbx_DB::connect($datasource); // attempt connection
         $db->query('SET NAMES utf8mb4');
-        $db->query('SET collation_connection = utf8mb4_unicode_ci');
+        //$db->query('SET collation_connection = utf8mb4_unicode_ci');
         break;
     case "sqlite":
         die_issabelpbx("SQLite2 support is deprecated. Please use sqlite3 only.");
