c96ad4f11567c8471a4ae58483c4d3a6ed7c59b3
Nicolás Gudiño <nicolas@house.com.ar>
2023-09-03 22:41:33 -0300

    Exit if there is no issabelpbx.conf file available

--- framework/amp_conf/bin/issabelpbx-cron-scheduler.php
+++ framework/amp_conf/bin/issabelpbx-cron-scheduler.php
@@ -3,7 +3,8 @@
 //include bootstrap
 $bootstrap_settings['issabelpbx_auth'] = false;
 if (!@include_once(getenv('ISSABELPBX_CONF') ? getenv('ISSABELPBX_CONF') : '/etc/issabelpbx.conf')) {
-	include_once('/etc/asterisk/issabelpbx.conf');
+    if(!is_readable('/etc/asterisk/issabelpbx.conf')) { die(); }
+    include_once('/etc/asterisk/issabelpbx.conf');
 }
 
 // Set language
@@ -30,47 +31,47 @@ $cm->run_jobs();
 $email = $cm->get_email();
 if ($email) {
 
-	$text="";
+    $text="";
 
-	// clear email flag
-	$nt->delete('issabelpbx', 'NOEMAIL');
+    // clear email flag
+    $nt->delete('issabelpbx', 'NOEMAIL');
 
-	// set to false, if no updates are needed then it will not be
-	// set to true and no email will go out even though the hash
-	// may have changed.
-	//
-	$send_email = false;
+    // set to false, if no updates are needed then it will not be
+    // set to true and no email will go out even though the hash
+    // may have changed.
+    //
+    $send_email = false;
 
-	$security = $nt->list_security();
-	if (count($security)) {
-		$send_email = true;
-		$text = "SECURITY NOTICE: ";
-		foreach ($security as $item) {
-			$text .= $item['display_text']."\n";
-			$text .= $item['extended_text']."\n\n";
-		}
-	}
-	$text .= "\n\n";
+    $security = $nt->list_security();
+    if (count($security)) {
+        $send_email = true;
+        $text = "SECURITY NOTICE: ";
+        foreach ($security as $item) {
+            $text .= $item['display_text']."\n";
+            $text .= $item['extended_text']."\n\n";
+        }
+    }
+    $text .= "\n\n";
 
-	$updates = $nt->list_update();
-	if (count($updates)) {
-		$send_email = true;
-		$text = "UPDATE NOTICE: ";
-		foreach ($updates as $item) {
-			$text .= $item['display_text']."\n";
-			$text .= $item['extended_text']."\n\n";
-		}
-	}
+    $updates = $nt->list_update();
+    if (count($updates)) {
+        $send_email = true;
+        $text = "UPDATE NOTICE: ";
+        foreach ($updates as $item) {
+            $text .= $item['display_text']."\n";
+            $text .= $item['extended_text']."\n\n";
+        }
+    }
 
-	if ($send_email && (! $cm->check_hash('update_email', $text))) {
-		$cm->save_hash('update_email', $text);
-		if (mail($email, __("IssabelPBX: New Online Updates Available"), $text)) {
-			$nt->delete('issabelpbx', 'EMAILFAIL');
-		} else {
-			$nt->add_error('issabelpbx', 'EMAILFAIL', __('Failed to send online update email'), sprintf(__('An attempt to send email to: %s with online update status failed'),$email));
-		}
-	}
+    if ($send_email && (! $cm->check_hash('update_email', $text))) {
+        $cm->save_hash('update_email', $text);
+        if (mail($email, __("IssabelPBX: New Online Updates Available"), $text)) {
+            $nt->delete('issabelpbx', 'EMAILFAIL');
+        } else {
+            $nt->add_error('issabelpbx', 'EMAILFAIL', __('Failed to send online update email'), sprintf(__('An attempt to send email to: %s with online update status failed'),$email));
+        }
+    }
 } else {
-		$nt->add_notice('issabelpbx', 'NOEMAIL', __('No email address for online update checks'), __('You are automatically checking for online updates nightly but you have no email address setup to send the results. This can be set in Module Admin. They will continue to show up here.'), '', 'PASSIVE', false);
+    $nt->add_notice('issabelpbx', 'NOEMAIL', __('No email address for online update checks'), __('You are automatically checking for online updates nightly but you have no email address setup to send the results. This can be set in Module Admin. They will continue to show up here.'), '', 'PASSIVE', false);
 }
 ?>
