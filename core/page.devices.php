ea76fec78da1197aa5f1f5894260fc86383e8f41
Nicolás Gudiño <nicolas@house.com.ar>
2023-09-26 20:08:08 -0300

    Update page.devices to new GUI format

--- core/page.devices.php
+++ core/page.devices.php
@@ -1,8 +1,14 @@
-<?php /* $Id$ */
-
+<?php
 if (!defined('ISSABELPBX_IS_AUTH')) { die('No direct script access allowed'); }
 ?>
-<?php 
+<?php
 $devices = core_devices_list();
-drawListMenu($devices, $type, $display, $extdisplay);
+if($devices===null) $devices=array();
+$rnaventries = array();
+foreach($devices as $idx=>$data) {
+    $midev = core_devices_get($data[0]);
+    $rnaventries[]=array($data[0],$data[1],'<span class="tag is-white tagfixed">'.$midev['tech'].'</span> '.$data[0]);
+}
+drawListMenu($rnaventries, $type, $display, $extdisplay);
 ?>
+<div class='content'>
