33573d18837073623c9110333dca58b7859ff35f
Nicolás Gudiño <nicolas@house.com.ar>
2023-10-04 18:11:03 -0300

    Extend extension to 45 for mysql too

--- queues/install.php
+++ queues/install.php
@@ -162,7 +162,7 @@ if(DB::IsError($results)) {
 	else  {
 		$sql = "
 		CREATE TABLE IF NOT EXISTS queues_config (
-		  extension varchar(20) NOT NULL default '',
+		  extension varchar(45) NOT NULL default '',
 		  descr varchar(35) NOT NULL default '',
 		  grppre varchar(100) NOT NULL default '',
 		  alertinfo varchar(254) NOT NULL default '',
