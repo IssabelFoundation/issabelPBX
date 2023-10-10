3d5809898be683f5c7f0fee59a9323038ffe89ca
Nicolás Gudiño <nicolas@house.com.ar>
2023-10-10 10:06:01 -0300

    Make descriptions translatable

--- callforward/install.php
+++ callforward/install.php
@@ -17,75 +17,75 @@ __("Call Forward Toggle");
 
 // Unconditional Call Forwarding
 $fcc = new featurecode('callforward', 'cfon');
-$fcc->setDescription('Call Forward All Activate');
+$fcc->setDescription(__('Call Forward All Activate'));
 $fcc->setDefault('*72');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfpon');
-$fcc->setDescription('Call Forward All Prompting Activate');
+$fcc->setDescription(__('Call Forward All Prompting Activate'));
 $fcc->setDefault('*720');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfoff');
-$fcc->setDescription('Call Forward All Deactivate');
+$fcc->setDescription(__('Call Forward All Deactivate'));
 $fcc->setDefault('*73');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfoff_any');
-$fcc->setDescription('Call Forward All Prompting Deactivate');
+$fcc->setDescription(__('Call Forward All Prompting Deactivate'));
 $fcc->setDefault('*74');
 $fcc->update();
 unset($fcc);
 
 // Call Forward on Busy
 $fcc = new featurecode('callforward', 'cfbon');
-$fcc->setDescription('Call Forward Busy Activate');
+$fcc->setDescription(__('Call Forward Busy Activate'));
 $fcc->setDefault('*90');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfbpon');
-$fcc->setDescription('Call Forward Busy Prompting Activate');
+$fcc->setDescription(__('Call Forward Busy Prompting Activate'));
 $fcc->setDefault('*900');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfboff');
-$fcc->setDescription('Call Forward Busy Deactivate');
+$fcc->setDescription(__('Call Forward Busy Deactivate'));
 $fcc->setDefault('*91');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfboff_any');
-$fcc->setDescription('Call Forward Busy Prompting Deactivate');
+$fcc->setDescription(__('Call Forward Busy Prompting Deactivate'));
 $fcc->setDefault('*92');
 $fcc->update();
 unset($fcc);
 
 // Call Forward on No Answer/Unavailable (i.e. phone not registered)
 $fcc = new featurecode('callforward', 'cfuon');
-$fcc->setDescription('Call Forward No Answer/Unavailable Activate');
+$fcc->setDescription(__('Call Forward No Answer/Unavailable Activate'));
 $fcc->setDefault('*52');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfupon');
-$fcc->setDescription('Call Forward No Answer/Unavailable Prompting Activate');
+$fcc->setDescription(__('Call Forward No Answer/Unavailable Prompting Activate'));
 $fcc->setDefault('*520');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cfuoff');
-$fcc->setDescription('Call Forward No Answer/Unavailable Deactivate');
+$fcc->setDescription(__('Call Forward No Answer/Unavailable Deactivate'));
 $fcc->setDefault('*53');
 $fcc->update();
 unset($fcc);
 
 $fcc = new featurecode('callforward', 'cf_toggle');
-$fcc->setDescription('Call Forward Toggle');
+$fcc->setDescription(__('Call Forward Toggle'));
 $fcc->setDefault('*740');
 $fcc->update();
 unset($fcc);
--- donotdisturb/install.php
+++ donotdisturb/install.php
@@ -10,21 +10,21 @@ __("DND Toggle");
 
 // Register FeatureCode - Activate
 $fcc = new featurecode('donotdisturb', 'dnd_on');
-$fcc->setDescription('DND Activate');
+$fcc->setDescription(__('DND Activate'));
 $fcc->setDefault('*78');
 $fcc->update();
 unset($fcc);
 
 // Register FeatureCode - Deactivate
 $fcc = new featurecode('donotdisturb', 'dnd_off');
-$fcc->setDescription('DND Deactivate');
+$fcc->setDescription(__('DND Deactivate'));
 $fcc->setDefault('*79');
 $fcc->update();
 unset($fcc);	
 
 // Register FeatureCode - Activate
 $fcc = new featurecode('donotdisturb', 'dnd_toggle');
-$fcc->setDescription('DND Toggle');
+$fcc->setDescription(__('DND Toggle'));
 $fcc->setDefault('*76');
 $fcc->update();
 unset($fcc);
