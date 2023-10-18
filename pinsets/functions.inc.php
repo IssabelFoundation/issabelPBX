89e814d58d8c3b7ace6570d83244200aa6d57895
German Venturino <venturinog@gmail.com>
2023-10-18 16:31:42 -0300

    Avoid empty addtocdr var
    Also in updates

--- pinsets/functions.inc.php
+++ pinsets/functions.inc.php
@@ -146,6 +146,7 @@ function pinsets_edit($id,$post){
 	extract($post);
 	$passwords = pinsets_clean($passwords);
 	if(empty($description)) $description = _('Unnamed');
+	if(empty($addtocdr)) $addtocdr = 0;
 	$results = sql("UPDATE pinsets SET description = \"$description\", passwords = \"$passwords\", addtocdr = \"$addtocdr\", deptname = \"$deptname\" WHERE pinsets_id = \"$id\"");
 }
 
